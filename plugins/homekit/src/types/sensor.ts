96f3d46deaf8f30a0a47e725445a86d69692cb62
Koushik Dutta <koushd@gmail.com>
2021-09-21 11:49:18 -0700

    homekit: standalone motion sensors

--- plugins/homekit/src/types/sensor.ts
+++ plugins/homekit/src/types/sensor.ts
@@ -1,21 +1,41 @@
 
-import { BinarySensor, ScryptedDevice, ScryptedDeviceType, ScryptedInterface } from '@scrypted/sdk'
+import { MotionSensor, BinarySensor, ScryptedDevice, ScryptedDeviceType, ScryptedInterface } from '@scrypted/sdk'
 import { addSupportedType, bindCharacteristic, DummyDevice } from '../common'
-import { Characteristic, Service } from '../hap';
+import { Characteristic, CharacteristicEventTypes, CharacteristicValue, NodeCallback, Service } from '../hap';
 import { makeAccessory } from './common';
 
 addSupportedType({
     type: ScryptedDeviceType.Sensor,
     probe(device: DummyDevice) {
-        return device.interfaces.includes(ScryptedInterface.BinarySensor);
+        return device.interfaces.includes(ScryptedInterface.BinarySensor) || device.interfaces.includes(ScryptedInterface.MotionSensor);
     },
-    getAccessory: (device: ScryptedDevice & BinarySensor) => {
+    getAccessory: (device: ScryptedDevice & BinarySensor & MotionSensor) => {
         const accessory = makeAccessory(device);
-        const service = accessory.addService(Service.ContactSensor, device.name);
-        service.getCharacteristic(Characteristic.ContactSensorState)
 
-        bindCharacteristic(device, ScryptedInterface.BinarySensor, service, Characteristic.ContactSensorState,
-            () => !!device.binaryState);
+        if (device.interfaces.includes(ScryptedInterface.BinarySensor)) {
+            const contactSensorService = accessory.addService(Service.ContactSensor, device.name);
+            contactSensorService.getCharacteristic(Characteristic.ContactSensorState)
+    
+            bindCharacteristic(device, ScryptedInterface.BinarySensor, contactSensorService, Characteristic.ContactSensorState,
+                () => !!device.binaryState);
+        }
+
+        if (device.interfaces.includes(ScryptedInterface.MotionSensor)) {
+            const motionSensorService = accessory.addService(Service.ContactSensor, device.name);
+            motionSensorService.getCharacteristic(Characteristic.MotionDetected)
+                .on(CharacteristicEventTypes.GET, (callback: NodeCallback<CharacteristicValue>) => {
+                    callback(null, !!device.motionDetected);
+                });
+    
+                device.listen({
+                event: ScryptedInterface.MotionSensor,
+                watch: false,
+            }, (eventSource, eventDetails, data) => {
+                motionSensorService.updateCharacteristic(Characteristic.MotionDetected, !!device.motionDetected);
+            });
+        }
+
+        // todo: more sensors.
 
         return accessory;
     }
