3405c4c8dad333677a3a13bc7539c41a5045da47
Koushik Dutta <koushd@gmail.com>
2021-09-30 23:08:22 -0700

    opencv install fixups

--- server/build-opencv4nodejs-linux.sh
+++ server/build-opencv4nodejs-linux.sh
@@ -1,8 +1,14 @@
-export OPENCV4NODEJS_DEFINES="remove_cv_t=remove_cv"
-export OPENCV4NODEJS_INCLUDES=/usr/local/Cellar/opencv@3/3.4.15/include/
-export OPENCV4NODEJS_BIN=/usr/local/Cellar/opencv@3/3.4.15/bin/
-export OPENCV4NODEJS_LIBRARIES="libopencv_core.a
-libopencv_videoio.a
-libopencv_objdetect.a"
+HAS_OPENCV_3=$(pkg-config --modversion opencv)
+if [ ! -z "$HAS_OPENCV_3" ]
+then
+    OPENCV_VERSION=opencv
+else
+    OPENCV_VERSION=opencv4
+fi
+echo $OPENCV_VERSION
+export CFLAGS=$(pkg-config --cflags $OPENCV_VERSION)
+export CXXFLAGS=$(pkg-config --cflags $OPENCV_VERSION)
+export LDFLAGS="-Wl,--no-as-needed $(pkg-config --libs $OPENCV_VERSION)"
+echo $LDFLAGS
 cd node_modules/@koush/opencv4nodejs
 npm run build
