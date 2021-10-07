770dd2fb93faad75800d1a4ccb0798eeaf536d46
Koushik Dutta <koushd@gmail.com>
2021-10-06 20:52:21 -0700

    sdk: convert to typescript

--- sdk/index.d.ts
+++ sdk/index.d.ts
@@ -1,129 +1,137 @@
-export * from './types'
-import { DeviceInformation, ScryptedInterface, ScryptedStatic, ScryptedDeviceType, Logger, ColorRgb, ColorHsv, DeviceState, TemperatureUnit, LockState, ThermostatMode, Position, ScryptedDevice  } from './types';
-
-export class ScryptedDeviceBase implements DeviceState {
-  constructor(nativeId?: string);
-  nativeId: string;
-  /**
-   * @deprecated
-   */
-  log: Logger;
-  console: Console;
-  storage: Storage;
-  id?: string;
-  interfaces?: string[];
-  mixins?: string[];
-  info?: DeviceInformation;
-  name?: string;
-  providedInterfaces?: string[];
-  providedName?: ScryptedDeviceType;
-  providedRoom?: string;
-  providedType?: ScryptedDeviceType;
-  providerId?: string;
-  room?: string;
-  type?: ScryptedDeviceType;
-  on?: boolean;
-  brightness?: number;
-  colorTemperature?: number;
-  rgb?: ColorRgb;
-  hsv?: ColorHsv;
-  running?: boolean;
-  paused?: boolean;
-  docked?: boolean;
-  /**
-   * Get the ambient temperature in Celsius.
-   */
-  temperature?: number;
-  /**
-   * Get the user facing unit of measurement for this thermometer. Note that while this may be Fahrenheit, getTemperatureAmbient will return the temperature in Celsius.
-   */
-  temperatureUnit?: TemperatureUnit;
-  humidity?: number;
-  thermostatAvailableModes?: ThermostatMode[];
-  thermostatMode?: ThermostatMode;
-  thermostatSetpoint?: number;
-  thermostatSetpointHigh?: number;
-  thermostatSetpointLow?: number;
-  lockState?: LockState;
-  entryOpen?: boolean;
-  batteryLevel?: number;
-  online?: boolean;
-  updateAvailable?: boolean;
-  fromMimeType?: string;
-  toMimeType?: string;
-  binaryState?: boolean;
-  intrusionDetected?: boolean;
-  powerDetected?: boolean;
-  motionDetected?: boolean;
-  audioDetected?: boolean;
-  occupied?: boolean;
-  flooded?: boolean;
-  ultraviolet?: number;
-  luminance?: number;
-  position?: Position;
+export * from './types';
+import type { ScryptedNativeId } from './types';
+import type { DeviceInformation, ScryptedInterface, ScryptedStatic, ScryptedDeviceType, Logger, ColorRgb, ColorHsv, DeviceState, TemperatureUnit, LockState, ThermostatMode, Position } from './types';
+export declare class ScryptedDeviceBase implements DeviceState {
+    nativeId: string;
+    id?: string;
+    interfaces?: string[];
+    mixins?: string[];
+    info?: DeviceInformation;
+    name?: string;
+    providedInterfaces?: string[];
+    providedName?: ScryptedDeviceType;
+    providedRoom?: string;
+    providedType?: ScryptedDeviceType;
+    providerId?: string;
+    room?: string;
+    type?: ScryptedDeviceType;
+    on?: boolean;
+    brightness?: number;
+    colorTemperature?: number;
+    rgb?: ColorRgb;
+    hsv?: ColorHsv;
+    running?: boolean;
+    paused?: boolean;
+    docked?: boolean;
+    /**
+     * Get the ambient temperature in Celsius.
+     */
+    temperature?: number;
+    /**
+     * Get the user facing unit of measurement for this thermometer. Note that while this may be Fahrenheit, getTemperatureAmbient will return the temperature in Celsius.
+     */
+    temperatureUnit?: TemperatureUnit;
+    humidity?: number;
+    thermostatAvailableModes?: ThermostatMode[];
+    thermostatMode?: ThermostatMode;
+    thermostatSetpoint?: number;
+    thermostatSetpointHigh?: number;
+    thermostatSetpointLow?: number;
+    lockState?: LockState;
+    entryOpen?: boolean;
+    batteryLevel?: number;
+    online?: boolean;
+    updateAvailable?: boolean;
+    fromMimeType?: string;
+    toMimeType?: string;
+    binaryState?: boolean;
+    intrusionDetected?: boolean;
+    powerDetected?: boolean;
+    motionDetected?: boolean;
+    audioDetected?: boolean;
+    occupied?: boolean;
+    flooded?: boolean;
+    ultraviolet?: number;
+    luminance?: number;
+    position?: Position;
+    private _storage;
+    private _log;
+    private _console;
+    private _deviceState;
+    constructor(nativeId: string);
+    get storage(): Storage;
+    /**
+     * @deprecated
+     */
+    get log(): Logger;
+    get console(): Console;
+    _lazyLoadDeviceState(): void;
 }
-
-
-export class MixinDeviceBase<T> implements DeviceState {
-  constructor(mixinDevice: T, mixinDeviceInterfaces: ScryptedInterface[], mixinDeviceState: { [key: string]: any }, mixinProviderNativeId: string);
-  mixinProviderNativeId: string;
-  mixinDevice: ScryptedDevice & T;
-  mixinDeviceInterfaces: ScryptedInterface[];
-  console: Console;
-  storage: Storage;
-  id?: string;
-  interfaces?: string[];
-  mixins?: string[];
-  info?: DeviceInformation;
-  name?: string;
-  providedInterfaces?: string[];
-  providedName?: ScryptedDeviceType;
-  providedRoom?: string;
-  providedType?: ScryptedDeviceType;
-  providerId?: string;
-  room?: string;
-  type?: ScryptedDeviceType;
-  on?: boolean;
-  brightness?: number;
-  colorTemperature?: number;
-  rgb?: ColorRgb;
-  hsv?: ColorHsv;
-  running?: boolean;
-  paused?: boolean;
-  docked?: boolean;
-  /**
-   * Get the ambient temperature in Celsius.
-   */
-  temperature?: number;
-  /**
-   * Get the user facing unit of measurement for this thermometer. Note that while this may be Fahrenheit, getTemperatureAmbient will return the temperature in Celsius.
-   */
-  temperatureUnit?: TemperatureUnit;
-  humidity?: number;
-  thermostatAvailableModes?: ThermostatMode[];
-  thermostatMode?: ThermostatMode;
-  thermostatSetpoint?: number;
-  thermostatSetpointHigh?: number;
-  thermostatSetpointLow?: number;
-  lockState?: LockState;
-  entryOpen?: boolean;
-  batteryLevel?: number;
-  online?: boolean;
-  updateAvailable?: boolean;
-  fromMimeType?: string;
-  toMimeType?: string;
-  binaryState?: boolean;
-  intrusionDetected?: boolean;
-  powerDetected?: boolean;
-  motionDetected?: boolean;
-  audioDetected?: boolean;
-  occupied?: boolean;
-  flooded?: boolean;
-  ultraviolet?: number;
-  luminance?: number;
-  position?: Position;
+export declare class MixinDeviceBase<T> implements DeviceState {
+    mixinDevice: any;
+    mixinDeviceInterfaces: ScryptedInterface[];
+    mixinProviderNativeId: ScryptedNativeId;
+    nativeId?: string;
+    id?: string;
+    interfaces?: string[];
+    mixins?: string[];
+    info?: DeviceInformation;
+    name?: string;
+    providedInterfaces?: string[];
+    providedName?: ScryptedDeviceType;
+    providedRoom?: string;
+    providedType?: ScryptedDeviceType;
+    providerId?: string;
+    room?: string;
+    type?: ScryptedDeviceType;
+    on?: boolean;
+    brightness?: number;
+    colorTemperature?: number;
+    rgb?: ColorRgb;
+    hsv?: ColorHsv;
+    running?: boolean;
+    paused?: boolean;
+    docked?: boolean;
+    /**
+     * Get the ambient temperature in Celsius.
+     */
+    temperature?: number;
+    /**
+     * Get the user facing unit of measurement for this thermometer. Note that while this may be Fahrenheit, getTemperatureAmbient will return the temperature in Celsius.
+     */
+    temperatureUnit?: TemperatureUnit;
+    humidity?: number;
+    thermostatAvailableModes?: ThermostatMode[];
+    thermostatMode?: ThermostatMode;
+    thermostatSetpoint?: number;
+    thermostatSetpointHigh?: number;
+    thermostatSetpointLow?: number;
+    lockState?: LockState;
+    entryOpen?: boolean;
+    batteryLevel?: number;
+    online?: boolean;
+    updateAvailable?: boolean;
+    fromMimeType?: string;
+    toMimeType?: string;
+    binaryState?: boolean;
+    intrusionDetected?: boolean;
+    powerDetected?: boolean;
+    motionDetected?: boolean;
+    audioDetected?: boolean;
+    occupied?: boolean;
+    flooded?: boolean;
+    ultraviolet?: number;
+    luminance?: number;
+    position?: Position;
+    private _storage;
+    private _log;
+    private _console;
+    private _deviceState;
+    constructor(mixinDevice: any, mixinDeviceInterfaces: ScryptedInterface[], mixinDeviceState: DeviceState, mixinProviderNativeId: ScryptedNativeId);
+    get storage(): Storage;
+    get console(): Console;
+    _lazyLoadDeviceState(): void;
+    release(): void;
 }
-
-declare const Scrypted: ScryptedStatic;
-
-export default Scrypted;
+declare let sdk: ScryptedStatic;
+export default sdk;
--- sdk/types.js
+++
@@ -1,3 +0,0 @@
-var types = require('./types.generated.js');
-module.exports = types;
-module.exports.default = types;
