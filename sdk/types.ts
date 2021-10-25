9b7dd5454c81dee73677a2e420135bf41cdcd93b
Koushik Dutta <koushd@gmail.com>
2021-10-24 21:00:32 -0700

    sdk/server: update

--- sdk/types.ts
+++ sdk/types.ts
@@ -1153,6 +1153,7 @@ export enum ScryptedInterfaceProperty {
   docked = "docked",
   thermostatAvailableModes = "thermostatAvailableModes",
   thermostatMode = "thermostatMode",
+  thermostatActiveMode = "thermostatActiveMode",
   thermostatSetpoint = "thermostatSetpoint",
   thermostatSetpointHigh = "thermostatSetpointHigh",
   thermostatSetpointLow = "thermostatSetpointLow",
