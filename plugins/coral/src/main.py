8d769176012c897f84be7b60209c816ff9aa28c0
Koushik Dutta <koushd@gmail.com>
2021-11-22 11:18:06 -0800

    coral: wip

--- plugins/coral/src/main.py
+++ plugins/coral/src/main.py
@@ -26,7 +26,7 @@ from gi.repository import Gst
 import multiprocessing
 
 from scrypted_sdk.types import FFMpegInput, Lock, MediaObject, ObjectDetection, ObjectDetectionModel, ObjectDetectionResult, ObjectDetectionSession, OnOff, ObjectsDetected, ScryptedInterface, ScryptedMimeTypes
-
+from scrypted_sdk import print
 
 def parse_label_contents(contents: str):
     lines = contents.splitlines()
@@ -43,8 +43,11 @@ def parse_label_contents(contents: str):
 class DetectionSession:
     id: str
     timerHandle: TimerHandle = None
-    future = Future()
+    future: Future
     loop: AbstractEventLoop
+
+    def __init__(self) -> None:
+        self.future = Future()
 
     def cancel(self):
         if self.timerHandle:
@@ -80,8 +83,14 @@ class CoralPlugin(scrypted_sdk.ScryptedDeviceBase, ObjectDetection):
 
     async def getInferenceModels(self) -> list[ObjectDetectionModel]:
         ret = list[ObjectDetectionModel]()
+        _, height, width, channels = self.interpreter.get_input_details()[
+            0]['shape']
+
         d = {
-            'classes': list(self.labels.values())
+            'id': 'mobilenet_ssd_v2_coco_quant_postprocess_edgetpu',
+            'name': 'Coco SSD',
+            'classes': list(self.labels.values()),
+            'inputShape': [int(width), int(height), int(channels)],
         }
         ret.append(d)
         return ret
@@ -110,6 +119,7 @@ class CoralPlugin(scrypted_sdk.ScryptedDeviceBase, ObjectDetection):
         detection_session.loop.create_task(self.onDeviceEvent(ScryptedInterface.ObjectDetection.value, detection_result))
 
     def end_session(self, detection_session: DetectionSession):
+        print('end session %s' % detection_session.id)
         detection_session.cancel()
         with self.session_mutex:
             del self.detection_sessions[detection_session.id]
@@ -120,6 +130,13 @@ class CoralPlugin(scrypted_sdk.ScryptedDeviceBase, ObjectDetection):
         self.detection_event(detection_session, detection_result)
 
     async def detectObjects(self, mediaObject: MediaObject, session: ObjectDetectionSession = None) -> ObjectsDetected:
+        if not mediaObject:
+            print("ended")
+            detection_id = session.get('detectionId', None)
+            detection_session = self.detection_sessions.get(detection_id, None)
+            self.end_session(detection_session)
+            return
+
         if mediaObject.mimeType.startswith('image/'):
             stream = io.BytesIO(bytes(await scrypted_sdk.mediaManager.convertMediaObjectToBuffer(mediaObject, 'image/jpeg')))
             image = Image.open(stream)
@@ -130,6 +147,36 @@ class CoralPlugin(scrypted_sdk.ScryptedDeviceBase, ObjectDetection):
             self.interpreter.invoke()
 
             return self.create_detection_result(image.size, scale)
+
+
+        duration = None
+        detection_id = None
+        new_session = False
+
+        if session:
+            detection_id = session.get('detectionId', None)
+            duration = session.get('duration', None)
+
+        if not detection_id:
+            detection_id = binascii.b2a_hex(os.urandom(15)).decode('utf8')
+
+        with self.session_mutex:
+            detection_session = self.detection_sessions.get(detection_id, None)
+            if not detection_session:
+                detection_session = DetectionSession()
+                detection_session.id = detection_id
+                loop = asyncio.get_event_loop()
+                detection_session.loop = loop
+                self.detection_sessions[detection_id] = detection_session
+                new_session = True
+
+                detection_session.future.add_done_callback(lambda _: self.end_session(detection_session))
+
+        if duration:
+            detection_session.setTimeout(duration / 1000)
+
+        if not new_session:
+            return
 
         b = await scrypted_sdk.mediaManager.convertMediaObjectToBuffer(mediaObject, ScryptedMimeTypes.MediaStreamUrl.value)
         s = b.decode('utf8')
@@ -149,36 +196,6 @@ class CoralPlugin(scrypted_sdk.ScryptedDeviceBase, ObjectDetection):
         w, h = (size['width'], size['height'])
         scale = min(width / w, height / h)
 
-        loop = asyncio.get_event_loop()
-
-        duration = None
-        detection_id = None
-        new_session = False
-
-        if session:
-            detection_id = session.get('detectionId', None)
-            duration = session.get('duration', None)
-
-        if not detection_id:
-            detection_id = binascii.b2a_hex(os.urandom(15)).decode('utf8')
-
-        with self.session_mutex:
-            detection_session = self.detection_sessions.get(detection_id, None)
-            if not detection_session:
-                detection_session = DetectionSession()
-                detection_session.id = detection_id
-                detection_session.loop = loop
-                self.detection_sessions[detection_id] = detection_session
-                new_session = True
-
-                detection_session.future.add_done_callback(lambda _: self.end_session(detection_session))
-
-        if duration:
-            detection_session.setTimeout(duration / 1000)
-
-        if not new_session:
-            return
-
         def user_callback(input_tensor, src_size, inference_box):
             with self.mutex:
                 run_inference(self.interpreter, input_tensor)
@@ -188,6 +205,7 @@ class CoralPlugin(scrypted_sdk.ScryptedDeviceBase, ObjectDetection):
             try:
                 detection_result = self.create_detection_result(
                     src_size, (scale, scale))
+                print(detection_result)
                 self.detection_event(detection_session, detection_result, mapinfo.data.tobytes())
 
                 if not session or not duration:
@@ -195,7 +213,6 @@ class CoralPlugin(scrypted_sdk.ScryptedDeviceBase, ObjectDetection):
             finally:
                 input_tensor.unmap(mapinfo)
 
-        print("creating pipeline %s", detection_id)
         pipeline = gstreamer.run_pipeline(detection_session.future, user_callback,
                                           src_size=(
                                               size['width'], size['height']),
