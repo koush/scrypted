81277dee64a9607106cf9a0f88f930b7f42c47fb
Koushik Dutta <koushd@gmail.com>
2021-12-07 17:44:19 -0800

    python: format code

--- server/python/plugin-remote.py
+++ server/python/plugin-remote.py
@@ -1,4 +1,22 @@
 from __future__ import annotations
+from typing import Optional
+from io import StringIO
+from typing import Any
+import subprocess
+import zipfile
+import time
+import base64
+from typing_extensions import TypedDict
+from typing import Tuple
+import aiofiles
+import json
+from asyncio.events import AbstractEventLoop
+import asyncio
+import rpc
+from genericpath import exists
+from collections.abc import Mapping
+from scrypted_python.scrypted_sdk.types import DeviceManifest, MediaManager, MediaObject, ScryptedInterfaceProperty
+import scrypted_python.scrypted_sdk
 from asyncio.futures import Future
 from asyncio.streams import StreamReader, StreamWriter
 import os
@@ -6,36 +24,20 @@ from os import sys
 from sys import stderr, stdout
 more = os.path.join(os.getcwd(), 'node_modules/@scrypted/sdk')
 sys.path.insert(0, more)
-import scrypted_python.scrypted_sdk
-from scrypted_python.scrypted_sdk.types import DeviceManifest, MediaManager, MediaObject, ScryptedInterfaceProperty
 
-from collections.abc import Mapping
-from genericpath import exists
-import rpc
-import asyncio
-from asyncio.events import AbstractEventLoop
-import json
-import aiofiles
-from typing import Tuple
-from typing_extensions import TypedDict
-import base64
-import time
-import zipfile
-import subprocess
-from typing import Any
-from io import StringIO
-from typing import Optional
 
 class SystemDeviceState(TypedDict):
     lastEventTime: int
     stateTime: int
     value: any
 
+
 class SystemManager(scrypted_python.scrypted_sdk.SystemManager):
     def __init__(self, api: Any, systemState: Mapping[str, Mapping[str, SystemDeviceState]]) -> None:
         super().__init__()
         self.api = api
         self.systemState = systemState
+
 
 class DeviceState(scrypted_python.scrypted_sdk.DeviceState):
     def __init__(self, id: str, nativeId: str, systemManager: SystemManager, deviceManager: scrypted_python.scrypted_sdk.DeviceManager) -> None:
@@ -46,7 +48,8 @@ class DeviceState(scrypted_python.scrypted_sdk.DeviceState):
         self.systemManager = systemManager
 
     def getScryptedProperty(self, property: str) -> Any:
-        deviceState = getattr(self.systemManager.systemState, self.nativeId, None)
+        deviceState = getattr(
+            self.systemManager.systemState, self.nativeId, None)
         if not deviceState:
             print("missing nativeId id %s" % self.nativeId)
             return None
@@ -58,7 +61,8 @@ class DeviceState(scrypted_python.scrypted_sdk.DeviceState):
         if property == ScryptedInterfaceProperty.mixins.value:
             raise Exception("mixins is read only")
         if property == ScryptedInterfaceProperty.interfaces.value:
-            raise Exception("interfaces is a read only post-mixin computed property, use providedInterfaces");
+            raise Exception(
+                "interfaces is a read only post-mixin computed property, use providedInterfaces")
 
         now = int(time.time() * 1000)
         self.systemManager.systemState[self._id][property] = {
@@ -75,12 +79,12 @@ class DeviceStorage:
     nativeId: str
     storage: Mapping[str, str] = {}
 
+
 class DeviceManager(scrypted_python.scrypted_sdk.DeviceManager):
     def __init__(self, nativeIds: Mapping[str, DeviceStorage], systemManager: SystemManager) -> None:
         super().__init__()
         self.nativeIds = nativeIds
         self.systemManager = systemManager
-
 
     def getDeviceState(self, nativeId: str) -> DeviceState:
         id = self.nativeIds[nativeId].id
@@ -99,6 +103,7 @@ class BufferSerializer(rpc.RpcSerializer):
 
     def deserialize(self, value):
         return base64.b64decode(value)
+
 
 class PluginRemote:
     systemState: Mapping[str, Mapping[str, SystemDeviceState]] = {}
@@ -121,15 +126,15 @@ class PluginRemote:
         ]
 
     async def print_async(self, nativeId: str, *values: object, sep: Optional[str] = ' ',
-            end: Optional[str] = '\n',
-            flush: bool = False,):
+                          end: Optional[str] = '\n',
+                          flush: bool = False,):
         consoleFuture = self.consoles.get(nativeId)
         if not consoleFuture:
             consoleFuture = Future()
             self.consoles[nativeId] = consoleFuture
             plugins = await self.api.getComponent('plugins')
             port = await plugins.getRemoteServicePort(self.pluginId, 'console-writer')
-            connection = await asyncio.open_connection(port = port)
+            connection = await asyncio.open_connection(port=port)
             _, writer = connection
             if not nativeId:
                 nid = 'undefined'
@@ -145,11 +150,11 @@ class PluginRemote:
         b = strio.read().encode('utf8')
         writer.write(b)
 
-
     def print(self, nativeId: str, *values: object, sep: Optional[str] = ' ',
-            end: Optional[str] = '\n',
-            flush: bool = False,):
-        asyncio.run_coroutine_threadsafe(self.print_async(nativeId, *values, sep=sep, end=end, flush=flush), self.loop)
+              end: Optional[str] = '\n',
+              flush: bool = False,):
+        asyncio.run_coroutine_threadsafe(self.print_async(
+            nativeId, *values, sep=sep, end=end, flush=flush), self.loop)
 
     async def loadZip(self, packageJson, zipData, options=None):
         zipPath = options['filename']
@@ -160,11 +165,13 @@ class PluginRemote:
 
         zip = zipfile.ZipFile(zipPath)
 
-        python_prefix = os.path.join(os.environ.get('SCRYPTED_PLUGIN_VOLUME'), 'python')
+        python_prefix = os.path.join(os.environ.get(
+            'SCRYPTED_PLUGIN_VOLUME'), 'python')
         if not os.path.exists(python_prefix):
             os.makedirs(python_prefix)
 
-        python = 'python%s' % str(sys.version_info[0])+"."+str(sys.version_info[1])
+        python = 'python%s' % str(
+            sys.version_info[0])+"."+str(sys.version_info[1])
         print('python:', python)
 
         if 'requirements.txt' in zip.namelist():
@@ -172,7 +179,8 @@ class PluginRemote:
             str_requirements = requirements.decode('utf8')
 
             requirementstxt = os.path.join(python_prefix, 'requirements.txt')
-            installed_requirementstxt = os.path.join(python_prefix, 'installed-requirements.txt')
+            installed_requirementstxt = os.path.join(
+                python_prefix, 'installed-requirements.txt')
 
             need_pip = True
             try:
@@ -189,7 +197,8 @@ class PluginRemote:
                 f.write(requirements)
                 f.close()
 
-                p = subprocess.Popen([python, '-m', 'pip', 'install', '-r', requirementstxt, '--prefix', python_prefix], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                p = subprocess.Popen([python, '-m', 'pip', 'install', '-r', requirementstxt,
+                                     '--prefix', python_prefix], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
                 while True:
                     line = p.stdout.readline()
                     if not line:
@@ -209,14 +218,16 @@ class PluginRemote:
                 print(str_requirements)
 
         sys.path.insert(0, zipPath)
-        site_packages = os.path.join(python_prefix, 'lib/%s/site-packages' % python)
+        site_packages = os.path.join(
+            python_prefix, 'lib/%s/site-packages' % python)
         sys.path.insert(0, site_packages)
-        from scrypted_sdk import sdk_init # type: ignore
+        from scrypted_sdk import sdk_init  # type: ignore
         self.systemManager = SystemManager(self.api, self.systemState)
         self.deviceManager = DeviceManager(self.nativeIds, self.systemManager)
         self.mediaManager = await self.api.getMediaManager()
-        sdk_init(zip, self, self.systemManager, self.deviceManager, self.mediaManager)
-        from main import create_scrypted_plugin # type: ignore
+        sdk_init(zip, self, self.systemManager,
+                 self.deviceManager, self.mediaManager)
+        from main import create_scrypted_plugin  # type: ignore
         return create_scrypted_plugin()
 
     async def setSystemState(self, state):
@@ -270,6 +281,7 @@ async def readLoop(loop, peer, reader):
             print('read loop error', e)
             pass
 
+
 async def async_main(loop: AbstractEventLoop):
     reader = await aiofiles.open(3, mode='r')
 
@@ -290,6 +302,7 @@ async def async_main(loop: AbstractEventLoop):
         api, pluginId, loop)
 
     await readLoop(loop, peer, reader)
+
 
 def main():
     loop = asyncio.get_event_loop()
