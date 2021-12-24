c08c6aded66f09e378e633a0c216c3109e9137d4
Koushik Dutta <koushd@gmail.com>
2021-12-23 23:21:00 -0800

    homekit: thermometer

--- plugins/homekit/src/types/sensor.ts
+++ plugins/homekit/src/types/sensor.ts
@@ -1,15 +1,15 @@
 
-import { MotionSensor, BinarySensor, ScryptedDevice, ScryptedDeviceType, ScryptedInterface } from '@scrypted/sdk'
+import { MotionSensor, BinarySensor, ScryptedDevice, ScryptedDeviceType, ScryptedInterface, Thermometer } from '@scrypted/sdk'
 import { addSupportedType, bindCharacteristic, DummyDevice } from '../common'
-import { Characteristic, CharacteristicEventTypes, CharacteristicValue, NodeCallback, Service } from '../hap';
+import { Characteristic, Service } from '../hap';
 import { makeAccessory } from './common';
 
 addSupportedType({
     type: ScryptedDeviceType.Sensor,
     probe(device: DummyDevice) {
-        return device.interfaces.includes(ScryptedInterface.BinarySensor) || device.interfaces.includes(ScryptedInterface.MotionSensor);
+        return device.interfaces.includes(ScryptedInterface.Thermometer) || device.interfaces.includes(ScryptedInterface.BinarySensor) || device.interfaces.includes(ScryptedInterface.MotionSensor);
     },
-    getAccessory: async (device: ScryptedDevice & BinarySensor & MotionSensor) => {
+    getAccessory: async (device: ScryptedDevice & BinarySensor & MotionSensor & Thermometer) => {
         const accessory = makeAccessory(device);
 
         if (device.interfaces.includes(ScryptedInterface.BinarySensor)) {
@@ -27,8 +27,14 @@ addSupportedType({
                 () => !!device.motionDetected, true);
         }
 
-        // todo: more sensors.
+        if (device.interfaces.includes(ScryptedInterface.Thermometer)) {
+            const service = accessory.addService(Service.TemperatureSensor, device.name);
+            bindCharacteristic(device, ScryptedInterface.Thermometer, service, Characteristic.CurrentTemperature,
+                () => device.temperature || 0);
 
+        }
+
+        // todo: more sensors.
         return accessory;
     }
 });
