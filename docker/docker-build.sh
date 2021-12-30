5529874243138aeee36c6d5554e7a57f18356285
Koushik Dutta <koush@koushikdutta.com>
2021-12-29 19:38:54 -0800

    docker: reduce layer

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -1,2 +1,2 @@
-docker build -t koush/scrypted-common -f Dockerfile.common . && \
-docker build -t koush/scrypted  -f Dockerfile .
+docker build -t koush/scrypted-common:16-bullseye -f Dockerfile.common --build-arg BASE=16-bullseye . && \
+docker build -t koush/scrypted:16-bullseye -f Dockerfile --build-arg BASE=16-bullseye .
