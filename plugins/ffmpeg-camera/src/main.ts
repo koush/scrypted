921cf0fcd6139a36f1b69c74fdb5774619885536
Koushik Dutta <koushd@gmail.com>
2022-01-26 11:20:35 -0800

    ffmpeg-camera: two way audio

--- plugins/ffmpeg-camera/src/main.ts
+++ plugins/ffmpeg-camera/src/main.ts
@@ -1,9 +1,53 @@
-import sdk, { FFMpegInput, MediaObject, MediaStreamOptions, Setting, SettingValue } from "@scrypted/sdk";
+import sdk, { FFMpegInput, Intercom, MediaObject, ScryptedInterface, ScryptedMimeTypes, Setting, SettingValue } from "@scrypted/sdk";
 import { CameraProviderBase, CameraBase, UrlMediaStreamOptions } from "./common";
+import { StorageSettings } from "../../../common/src/settings";
+import { ffmpegLogInitialOutput, safePrintFFmpegArguments } from "../../../common/src/media-helpers";
+import child_process, { ChildProcess } from "child_process";
 
 const { log, deviceManager, mediaManager } = sdk;
 
-class FFmpegCamera extends CameraBase<UrlMediaStreamOptions> {
+class FFmpegCamera extends CameraBase<UrlMediaStreamOptions> implements Intercom {
+    storageSettings = new StorageSettings(this, {
+        ffmpegInputs: {
+            title: 'FFmpeg Input Stream Arguments',
+            description: 'FFmpeg input arguments passed to the command line ffmpeg tool. A camera may have multiple streams with different bitrates.',
+            placeholder: '-i rtmp://[user:password@]192.168.1.100[:1935]/channel/101',
+            multiple: true,
+        },
+        ffmpegOutput: {
+            title: 'FFmpeg Output Stream Arguments',
+            description: 'Optional (two way audio): FFmpeg output arguments passed to the command line ffmpeg tool to play back an audio stream.',
+            placeholder: '-vn -acodec copy -f adts udp://192.168.1.101:1234',
+            onPut: (_, newValue) => {
+                let interfaces = this.providedInterfaces;
+                if (!newValue)
+                    interfaces = interfaces.filter(iface => iface !== ScryptedInterface.Intercom);
+                else
+                    interfaces.push(ScryptedInterface.Intercom);
+                this.provider.updateDevice(this.nativeId, this.providedName, interfaces);
+            },
+        },
+    })
+
+    twoway: ChildProcess;
+
+    async startIntercom(media: MediaObject): Promise<void> {
+        const buffer = await mediaManager.convertMediaObjectToBuffer(media, ScryptedMimeTypes.FFmpegInput);
+        const ffmpegInput: FFMpegInput = JSON.parse(buffer.toString());
+
+        const args = ffmpegInput.inputArguments.slice();
+        args.push(...this.storageSettings.values.ffmpegOutput.split(' '));
+        this.console.log('starting intercom', safePrintFFmpegArguments(this.console, args));
+        this.stopIntercom();
+        this.twoway = child_process.spawn(await mediaManager.getFFmpegPath(), args);
+        ffmpegLogInitialOutput(this.console, this.twoway);
+    }
+
+    async stopIntercom(): Promise<void> {
+        this.twoway?.kill('SIGKILL');
+        this.twoway = undefined;
+    }
+
     createFFmpegMediaStreamOptions(ffmpegInput: string, index: number) {
         // this might be usable as a url so check that.
         let url: string;
@@ -23,19 +67,8 @@ class FFmpegCamera extends CameraBase<UrlMediaStreamOptions> {
         };
     }
 
-    getFFmpegInputs() {
-        let ffmpegInputs: string[] = [];
-        try {
-            ffmpegInputs = JSON.parse(this.storage.getItem('ffmpegInputs'));
-        }
-        catch (e) {
-        }
-
-        return ffmpegInputs;
-    }
-
     getRawVideoStreamOptions(): UrlMediaStreamOptions[] {
-        const ffmpegInputs = this.getFFmpegInputs();
+        const ffmpegInputs = this.storageSettings.values.ffmpegInputs;
 
         // filter out empty strings.
         const ret = ffmpegInputs
@@ -49,29 +82,16 @@ class FFmpegCamera extends CameraBase<UrlMediaStreamOptions> {
     }
 
     async getFFmpegInputSettings(): Promise<Setting[]> {
-        return [
-            {
-                key: 'ffmpegInputs',
-                title: 'FFmpeg Input Stream Arguments',
-                description: 'FFmpeg input arguments passed to the command line ffmpeg tool. A camera may have multiple streams with different bitrates.',
-                placeholder: '-i rtmp://[user:password@]192.168.1.100[:1935]/channel/101',
-                value: this.getFFmpegInputs(),
-                multiple: true,
-            },
-        ];
+        return this.storageSettings.getSettings();
     }
 
     async putSettingBase(key: string, value: SettingValue) {
-        if (key === 'ffmpegInputs') {
-            this.putFFmpegInputs(value as string[]);
+        if (this.storageSettings.settings[key]) {
+            this.storageSettings.putSetting(key, value);
         }
         else {
             super.putSettingBase(key, value);
         }
-    }
-
-    async putFFmpegInputs(urls: string[]) {
-        this.storage.setItem('ffmpegInputs', JSON.stringify(urls.filter(url => !!url)));
     }
 
     async getUrlSettings(): Promise<Setting[]> {
@@ -83,7 +103,7 @@ class FFmpegCamera extends CameraBase<UrlMediaStreamOptions> {
 
     async createVideoStream(options?: UrlMediaStreamOptions): Promise<MediaObject> {
         const index = this.getRawVideoStreamOptions()?.findIndex(vso => vso.id === options.id);
-        const ffmpegInputs = this.getFFmpegInputs();
+        const ffmpegInputs = this.storageSettings.values.ffmpegInputs;
         const ffmpegInput = ffmpegInputs[index];
 
         if (!ffmpegInput)
