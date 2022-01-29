6983157dd5f296a1369d8d64c21db23be5069c6e
Austin Drummond <adrum0x7@gmail.com>
2022-01-29 15:32:12 -0500

    add launchd for macos

--- docker/install-scrypted-dependencies-mac.sh
+++ docker/install-scrypted-dependencies-mac.sh
@@ -30,6 +30,47 @@ RUN pip3 install aiofiles debugpy typing_extensions typing opencv-python
 echo "Installing Scrypted..."
 RUN npx -y scrypted install-server
 
+RUN mkdir -p ~/Library/LaunchAgents
+RUN cat << EOF | tee -a ~/Library/LaunchAgents/com.scrypted.server.plist
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+    <key>RunAtLoad</key>
+        <true/>
+    <key>KeepAlive</key>
+        <true/>
+    <key>Label</key>
+        <string>com.scrypted.server</string>
+    <key>ProgramArguments</key>
+        <array>
+             <string>$(brew --prefix)/bin/npx</string>
+             <string>-y</string>
+             <string>scrypted</string>
+             <string>serve</string>
+        </array>
+    <key>WorkingDirectory</key>
+         <string>/Users/$(whoami)/.scrypted</string>
+    <key>StandardOutPath</key>
+        <string>/Users/$(whoami)/.scrypted/scrypted.log</string>
+    <key>StandardErrorPath</key>
+        <string>/Users/$(whoami)/.scrypted/scrypted.log</string>
+    <key>UserName</key>
+        <string>$(whoami)</string>
+    <key>EnvironmentVariables</key>
+        <dict>
+            <key>PATH</key>
+                <string>$(brew --prefix)/bin:$(brew --prefix)/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin</string>
+            <key>HOME</key>
+                <string>/Users/$(whoami)</string>
+        </dict>
+</dict>
+</plist>
+EOF
+
+RUN launchctl load ~/Library/LaunchAgents/com.scrypted.server.plist
+RUN launchctl start ~/Library/LaunchAgents/com.scrypted.server.plist
+
 set +x
 echo
 echo
