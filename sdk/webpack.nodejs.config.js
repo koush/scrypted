2473c5920834a3046ccb500aed0caea13bc5fb8c
Koushik Dutta <koushd@gmail.com>
2022-02-11 11:35:15 -0800

    update externals

--- sdk/webpack.nodejs.config.js
+++ sdk/webpack.nodejs.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const TerserPlugin = require("terser-webpack-plugin");
-// const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const fs = require('fs');
 
 let out;
