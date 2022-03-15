00268aa0e229e62b9701195b3bdf934aeadba380
Koushik Dutta <koushd@gmail.com>
2022-03-14 21:23:17 -0700

    homekit: revert tree shaking

--- plugins/homekit/src/hap.ts
+++ plugins/homekit/src/hap.ts
@@ -1,14 +1,8 @@
-export * from 'hap-nodejs/src/lib/definitions'; // must be loaded before Characteristic and Service class
-export * from 'hap-nodejs/src/lib/Characteristic';
+export * from 'hap-nodejs/src';
+export * from 'hap-nodejs/src/index';
 export * from 'hap-nodejs/src/lib/camera';
 export * from 'hap-nodejs/src/lib/camera/RecordingManagement';
+export * from "hap-nodejs/src/lib/definitions";
 export * from 'hap-nodejs/src/lib/model/ControllerStorage';
 export * from 'hap-nodejs/src/lib/util/eventedhttp';
-export * from 'hap-nodejs/src/lib/controller/CameraController';
-export * from 'hap-nodejs/src/lib/datastream/DataStreamServer';
-export * from 'hap-nodejs/src/lib/Service';
-export * from 'hap-nodejs/src/types';
-export * from 'hap-nodejs/src/lib/Accessory';
-export * as uuid from 'hap-nodejs/src/lib/util/uuid';
-export * from 'hap-nodejs/src/lib/model/HAPStorage';
-export * from 'hap-nodejs/src/lib/Bridge';
+
