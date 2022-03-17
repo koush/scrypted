89167e09a31e430da02aaeabd2c86b5412c9b441
Koushik Dutta <koushd@gmail.com>
2022-03-16 21:56:26 -0700

    core: use relative urls for clips

--- plugins/core/ui/src/common/videoclip.ts
+++ plugins/core/ui/src/common/videoclip.ts
@@ -6,11 +6,11 @@ import { VideoClip } from "@scrypted/types";
 export async function fetchClipThumbnail(mediaManager: MediaManager, device: VideoClips, clip: VideoClip) {
     const mo = await device.getVideoClipThumbnail(clip.id);
     const url = await mediaManager.convertMediaObject(mo, ScryptedMimeTypes.LocalUrl);
-    return url.toString();
+    return new URL(url.toString()).pathname;
 }
 
 export async function fetchClipUrl(mediaManager: MediaManager, device: VideoClips, clip: VideoClip) {
     const mo = await device.getVideoClip(clip.id);
     const url = await mediaManager.convertMediaObject(mo, ScryptedMimeTypes.LocalUrl);
-    return url.toString();
+    return new URL(url.toString()).pathname;
 }
