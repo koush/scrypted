9a444ba275e8315931291ee28793a9bef2f78f19
Brett Jia <dev.bjia56@gmail.com>
2022-03-19 08:45:58 -0400

    log formatter

--- plugins/arlo/src/arlo_plugin/logging.py
+++ plugins/arlo/src/arlo_plugin/logging.py
@@ -11,7 +11,7 @@ def getLogger(name):
     handler = logging.StreamHandler(sys.stdout)
     handler.setLevel(DEFAULT_LOG_LEVEL)
 
-    formatter = logging.Formatter('[%(asctime)s - %(levelname)s - %(name)s] %(message)s')
+    formatter = logging.Formatter('[%(asctime)s %(levelname)s %(name)s] %(message)s')
     handler.setFormatter(formatter)
     logger.addHandler(handler)
     return logger
