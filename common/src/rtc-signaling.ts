1ed1f14b06a2373fad65324ffe00ccb140d42c28
Koushik Dutta <koushd@gmail.com>
2022-04-04 10:36:47 -0700

    common: fix paths

--- common/src/rtc-signaling.ts
+++ common/src/rtc-signaling.ts
@@ -1,3 +1,4 @@
+import { RpcPeer } from "@scrypted/server/src/rpc";
 import type { RTCSignalingSendIceCandidate, RTCSignalingSession, RTCAVSignalingSetup, RTCSignalingOptions } from "@scrypted/sdk/types";
 // import type { RTCPeerConnection as WeriftRTCPeerConnection } from "@koush/werift";
 
@@ -206,4 +207,23 @@ export class BrowserSignalingSession implements RTCSignalingSession {
 
     async endSession() {
     }
+}
+
+export async function createBrowserSignalingSession(ws: WebSocket, localName: string, remoteName: string) {
+    const peer = new RpcPeer(localName, remoteName, (message, reject) => {
+        const json = JSON.stringify(message);
+        try {
+            ws.send(json);
+        }
+        catch (e) {
+            reject?.(e);
+        }
+    });
+    ws.onmessage = message => {
+        const json = JSON.parse(message.data);
+        peer.handleMessage(json);
+    };
+
+    const session: RTCSignalingSession = await peer.getParam('session');
+    return session;
 }
