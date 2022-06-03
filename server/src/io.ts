e15f8bc0e3274d0c1e1c2c0770b0bd0f0d48aff2
Koushik Dutta <koush@Koushik-MacStudio.local>
2022-06-03 13:37:55 -0700

    server: fix io types

--- server/src/io.ts
+++ server/src/io.ts
@@ -1,27 +1,25 @@
 import type { Server, Socket as ServerSocket } from "engine.io";
-import type { Socket as ClientSocket } from "engine.io-client";
 
 export type IOServer = {
     on(ev: 'connection' | 'drain', fn: (socket: IOServerSocket & IOServerSocket) => void): IOServer;
 } & Server;
 
 export type IOServerSocket = IOSocket & ServerSocket;
-export type IOClientSocket = IOSocket & ClientSocket;
 
-export type IOSocket = {
-    send(data: any, options?: any, callback?: any): IOServerSocket;
+export interface IOSocket {
+    send(data: any, options?: any, callback?: any): this;
 
-    on(ev: "close", fn: (reason: string, description?: Error) => void): IOServerSocket;
+    on(ev: "close", fn: (reason: string, description?: Error) => void): this;
     /**
      * Fired when the client sends a message.
      */
-    on(ev: "message", fn: (data: string | Buffer) => void): IOServerSocket;
+    on(ev: "message", fn: (data: string | Buffer) => void): this;
     /**
      * Fired when an error occurs.
      */
-    on(ev: "error", fn: (err: Error) => void): IOServerSocket;
+    on(ev: "error", fn: (err: Error) => void): this;
     /**
      * Called when the write buffer is drained
      */
-    on(ev: "drain", fn: () => void): IOServerSocket;
+    on(ev: "drain", fn: () => void): this;
 }
