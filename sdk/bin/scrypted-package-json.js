ed5429d7ff61b4a38cf18ac49ab16adc0de3755c
Koushik Dutta <koush@Koushik-MacStudio.localdomain>
2022-06-17 09:25:29 -0700

    sdk: update

--- sdk/bin/scrypted-package-json.js
+++ sdk/bin/scrypted-package-json.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const pkg = JSON.parse(fs.readFileSync('package.json'));
 pkg.scripts = {
     // alias
+    "postinstall": "scrypted-setup-project",
     "build": "scrypted-webpack",
     "prepublishOnly": "NODE_ENV=production scrypted-webpack",
     "prescrypted-vscode-launch": "scrypted-webpack",
@@ -13,6 +14,5 @@ pkg.scripts = {
     "scrypted-deploy": "scrypted-deploy",
     "scrypted-readme": "scrypted-readme",
     "scrypted-package-json": "scrypted-package-json",
-    "scrypted-webpack": "scrypted-webpack"
- };
- fs.writeFileSync('package.json', JSON.stringify(pkg, null, 3) + '\n');
+};
+fs.writeFileSync('package.json', JSON.stringify(pkg, null, 3) + '\n');
