6d7a0ffee8d9dedb107485f00e6167c02eb6a221
Billy Zoellers <billy.zoellers@mac.com>
2022-08-06 20:29:04 -0400

    separate motion sensor and tamper sensor

--- plugins/zwave/src/CommandClasses/TamperSensorToHomeSecurity.ts
+++ plugins/zwave/src/CommandClasses/TamperSensorToHomeSecurity.ts
@@ -1,14 +1,26 @@
-import { TamperSensor } from "@scrypted/sdk";
-import { ZWaveNodeValueUpdatedArgs } from "zwave-js";
+import { ScryptedInterface, TamperSensor, MotionSensor } from "@scrypted/sdk";
+import { ZWaveNode, ZWaveNodeValueUpdatedArgs } from "zwave-js";
+import { ValueID } from "@zwave-js/core";
 import { Notification } from "./Notification";
 import { ZwaveDeviceBase } from "./ZwaveDeviceBase";
 
 
-export class TamperSensorToHomeSecurity extends Notification implements TamperSensor {
+export class TamperSensorToHomeSecurity extends Notification implements TamperSensor, MotionSensor {
     static updateState(zwaveDevice: ZwaveDeviceBase, valueId: ZWaveNodeValueUpdatedArgs) {
         const notification = Notification.lookupNotification(zwaveDevice, 'Home Security');
         const value  = notification.lookupValue(valueId.newValue as number);
-        // unset with any noninstrusion value
-        zwaveDevice.tampered = !!value;
+
+        if (valueId.propertyKey === "Motion sensor status")
+            zwaveDevice.motionDetected = !!value;
+        else
+            zwaveDevice.tampered = !!value;
+    }
+
+    static getInterfaces(node: ZWaveNode, valueId: ValueID): string[] {
+        const interfaces = [ScryptedInterface.TamperSensor];
+        if (Notification.checkInterface(node, valueId, "Motion detection")) {
+            interfaces.push(ScryptedInterface.MotionSensor);
+        }
+        return interfaces;
     }
 }
