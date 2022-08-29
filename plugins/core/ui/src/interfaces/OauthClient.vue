9f57969690f78b545f3b5b1eda21d8ea5eaad03d
Koushik Dutta <koush@Koushik-MacStudio.localdomain>
2022-08-29 11:22:29 -0700

    core: support custom and relative path redirect uri for oauth clients

--- plugins/core/ui/src/interfaces/OauthClient.vue
+++ plugins/core/ui/src/interfaces/OauthClient.vue
@@ -8,14 +8,32 @@ import qs from 'query-string';
 export default {
   mixins: [RPCInterface],
   methods: {
-    onChange() {},
-    onClick: function() {
+    onChange() { },
+    onClick: function () {
       this.rpc()
         .getOauthUrl()
         .then(data => {
           var url = new URL(data);
           var querystring = qs.parse(url.search.replace("?", ""));
-          querystring.redirect_uri = `https://home.scrypted.app/web/oauth/callback`;
+          let { redirect_uri } = querystring;
+          if (redirect_uri) {
+            let u;
+            try {
+              u = new URL(redirect_uri);
+            }
+            catch (e) {
+              u = new URL(redirect_uri, window.location.href);
+              u.hostname = 'localhost';
+            }
+            if (u.hostname === 'localhost') {
+              u.hostname = new URL(window.location.href).hostname;
+              redirect_uri = u.toString();
+            }
+          }
+          else {
+            redirect_uri = `https://home.scrypted.app/web/oauth/callback`;
+          }
+          querystring.redirect_uri = redirect_uri;
           querystring.state = JSON.stringify({
             d: this.device.id,
             s: querystring.state,
