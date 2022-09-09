aaf2529ec04d88f1532412e7867a667170776697
Koushik Dutta <koush@Koushik-MacStudio.localdomain>
2022-09-09 13:15:45 -0700

    server: publish

--- server/scripts/github-workflow-publish-docker.sh
+++ server/scripts/github-workflow-publish-docker.sh
@@ -1,2 +1,2 @@
 cd $(dirname $0)
-gh workflow run docker.yml -f PACKAGE_JSON_VERSION=$(node print-package-json-version.js)
+gh workflow run docker.yml -f package_version=$(node print-package-json-version.js)
