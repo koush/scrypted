a2335930b75adab239321e316ef2fc241c3fcf92
Koushik Dutta <koush@Koushik-MacStudio.localdomain>
2022-10-18 11:48:31 -0700

    server: decouple docker from npm publish. docker builds must now specify latest tag.

--- server/scripts/github-workflow-publish-docker.sh
+++ server/scripts/github-workflow-publish-docker.sh
@@ -1,2 +1,7 @@
 cd $(dirname $0)
-gh workflow run docker.yml -f package_version=$(node print-package-json-version.js) -f docker_tag=beta
+if [ -z "$1" ]
+then
+    echo "Docker tag not specified"
+    exit 1
+fi
+gh workflow run docker.yml -f package_version=$(node print-package-json-version.js) -f docker_tag=$1
