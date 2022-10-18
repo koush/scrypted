b1d17828e47bf838a6d48e7f4293b1a64766ef3d
Koushik Dutta <koush@Koushik-MacStudio.localdomain>
2022-10-18 11:24:06 -0700

    server: test beta publish

--- server/scripts/github-workflow-publish-docker.sh
+++ server/scripts/github-workflow-publish-docker.sh
@@ -1,2 +1,2 @@
 cd $(dirname $0)
-gh workflow run docker.yml -f package_version=$(node print-package-json-version.js)
+gh workflow run docker.yml -f package_version=$(node print-package-json-version.js) -f docker_tag=beta
