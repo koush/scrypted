4a9179ffd35811deb9bf50a36485d2d458df7fe9
Koushik Dutta <koush@Koushik-MacStudio.local>
2022-10-25 12:41:15 -0700

    mqtt: add button example

--- plugins/mqtt/fs/examples/button.ts
+++ plugins/mqtt/fs/examples/button.ts
@@ -7,9 +7,8 @@ declare const mqtt: MqttClient; // SCRYPTED_FILTER_EXAMPLE_LINE
 mqtt.subscribe({
     // this example expects the device to publish either ON or OFF text values
     // to the mqtt endpoint.
-    'topic/motion': value => {
-        return device.motionDetected = value.text === 'ON';
+    'topic/button': value => {
+        return device.binaryState = value.text === 'ON';
     },
 });
-
-mqtt.handleTypes(ScryptedInterface.MotionSensor);
+mqtt.handleTypes(ScryptedInterface.BinarySensor);
