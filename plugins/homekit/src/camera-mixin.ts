c74963d09503ab9cdb70a644ebd292a8c1dbc388
Koushik Dutta <koush@Koushik-MacStudio.local>
2022-11-07 20:28:57 -0800

    homekit: Direct users to troubleshooting guide in camera readme

--- plugins/homekit/src/camera-mixin.ts
+++ plugins/homekit/src/camera-mixin.ts
@@ -51,7 +51,11 @@ More details can be found [here](https://github.com/koush/scrypted/blob/main/plu
         readme += `
 ## HomeKit Codec Settings
 
-The recommended codec settings for cameras in HomeKit can be viewed in the [HomeKit plugin](#/device/${id}).`
+The recommended codec settings for cameras in HomeKit can be viewed in the [HomeKit plugin](#/device/${id}).
+
+## Troubleshooting
+
+The latest troubleshooting guide for all known streaming or recording issues can be viewed in the [HomeKit plugin](#/device/${id}).`;
 
         return readme;
     }
