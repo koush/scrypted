ddaa34b2ff50c744d434d15f9ae7599e76169954
Koushik Dutta <koush@Koushik-MacStudio.localdomain>
2022-11-23 08:16:31 -0800

    docker: refactor

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -1,4 +1,5 @@
 ./template/generate-dockerfile.sh
 
-docker build -t koush/scrypted-common:16-bullseye -f Dockerfile.common --build-arg BASE=16-bullseye . && \
-docker build -t koush/scrypted:16-bullseye -f Dockerfile --build-arg BASE=16-bullseye .
+docker build -t koush/scrypted-common:16-bullseye -f Dockerfile.common --build-arg NODE_VERSION=16 . && \
+docker build -t koush/scrypted-common:16-bullseye-s6 -f Dockerfile.common.s6 --build-arg BASE=16-bullseye . && \
+docker build -t koush/scrypted:16-bullseye-s6 -f Dockerfile --build-arg BASE=16-bullseye-s6 .
