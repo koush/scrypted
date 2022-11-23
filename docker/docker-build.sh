ee86b82656bb254104aa06c8c4bee062142b84e6
Koushik Dutta <koush@koushikdutta.com>
2022-11-23 10:48:27 -0800

    docker: wip

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -6,13 +6,12 @@ NODE_VERSION=18
 BUILDPACK_DEPS_BASE=bullseye
 FLAVOR=full
 BASE=$NODE_VERSION-$BUILDPACK_DEPS_BASE-$FLAVOR
-S6_BASE=$BASE-s6
+echo $BASE
+SUPERVISOR=.s6
+SUPERVISOR_BASE=$BASE$SUPERVISOR
 
 docker build -t koush/scrypted-common:$BASE -f Dockerfile.$FLAVOR \
     --build-arg NODE_VERSION=$NODE_VERSION --build-arg BUILDPACK_DEPS_BASE=$BUILDPACK_DEPS_BASE . && \
 \
-docker build -t koush/scrypted-common:$S6_BASE -f Dockerfile.s6 \
-    --build-arg BASE=$BASE . && \
-\
-docker build -t koush/scrypted:$S6_BASE -f Dockerfile \
-    --build-arg BASE=$S6_BASE .
+docker build -t koush/scrypted:$SUPERVISOR_BASE -f Dockerfile$SUPERVISOR \
+    --build-arg BASE=$BASE .
