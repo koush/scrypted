a61618fb8f619ccec5195a7ec8e2b3ffdd6952fc
Koushik Dutta <koush@koushikdutta.com>
2022-11-23 09:46:28 -0800

    docker: fix labels

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -1,5 +1,15 @@
 ./template/generate-dockerfile.sh
 
-docker build -t koush/scrypted-common:16-bullseye -f Dockerfile.common --build-arg NODE_VERSION=16 . && \
-docker build -t koush/scrypted-common:16-bullseye-s6 -f Dockerfile.common.s6 --build-arg BASE=16-bullseye . && \
-docker build -t koush/scrypted:16-bullseye-s6 -f Dockerfile --build-arg BASE=16-bullseye-s6 .
+BUILDPACK_DEPS_BASE=bullseye
+NODE_VERSION=18
+BASE=$BUILDPACK_DEPS_BASE-$NODE_VERSION
+S6_BASE=$BASE.s6
+
+docker build -t koush/scrypted-common:$BASE -f Dockerfile.common \
+    --build-arg NODE_VERSION=$NODE_VERSION --build-arg BUILDPACK_DEPS_BASE=$BUILDPACK_DEPS_BASE . && \
+\
+docker build -t koush/scrypted-common:$S6_BASE -f Dockerfile.common.s6 \
+    --build-arg BASE=$BASE . && \
+\
+docker build -t koush/scrypted:$S6_BASE -f Dockerfile \
+    --build-arg BASE=$S6_BASE .
