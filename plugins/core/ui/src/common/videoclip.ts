f3f370d5e064cb0d3824ee6f9cf59f7ceec6ec71
Koushik Dutta <koushd@gmail.com>
2022-11-30 14:04:37 -0800

    core/ui: use relative paths in video clip downloads if possible

--- plugins/core/ui/src/common/videoclip.ts
+++ plugins/core/ui/src/common/videoclip.ts
@@ -6,11 +6,23 @@ import { VideoClip } from "@scrypted/types";
 export async function fetchClipThumbnail(mediaManager: MediaManager, device: VideoClips, clip: VideoClip) {
     const mo = await device.getVideoClipThumbnail(clip.thumbnailId || clip.id);
     const url = await mediaManager.convertMediaObject(mo, ScryptedMimeTypes.LocalUrl);
-    return url.toString();
+    try {
+        const u = new URL(url.toString());
+        return u.pathname + '?' + u.search;
+    }
+    catch (e) {
+        return url;
+    }
 }
 
 export async function fetchClipUrl(mediaManager: MediaManager, device: VideoClips, clip: VideoClip) {
     const mo = await device.getVideoClip(clip.videoId || clip.id);
     const url = await mediaManager.convertMediaObject(mo, ScryptedMimeTypes.LocalUrl);
-    return url.toString();
+    try {
+        const u = new URL(url.toString());
+        return u.pathname + '?' + u.search;
+    }
+    catch (e) {
+        return url;
+    }
 }
