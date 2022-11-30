690387f7550bdfb8120088d5d58e0e80c19539b9
Koushik Dutta <koushd@gmail.com>
2022-11-30 14:30:35 -0800

    homekit: do not allow snapshot pileups

--- plugins/homekit/src/types/camera/camera-snapshot.ts
+++ plugins/homekit/src/types/camera/camera-snapshot.ts
@@ -1,6 +1,6 @@
-import { TimeoutError, timeoutPromise } from "@scrypted/common/src/promise-utils";
+import { sleep } from "@scrypted/common/src/sleep";
 import sdk, { AudioSensor, Camera, Intercom, Logger, MotionSensor, ScryptedDevice, ScryptedInterface, VideoCamera } from "@scrypted/sdk";
-import throttle from 'lodash/throttle';
+import throttle from "lodash/throttle";
 import { SnapshotRequest, SnapshotRequestCallback } from "../../hap";
 import type { HomeKitPlugin } from "../../main";
 
@@ -15,17 +15,12 @@ function recommendSnapshotPlugin(console: Console, log: Logger, message: string)
 
 export function createSnapshotHandler(device: ScryptedDevice & VideoCamera & Camera & MotionSensor & AudioSensor & Intercom, storage: Storage, homekitPlugin: HomeKitPlugin, console: Console) {
     let pendingPicture: Promise<Buffer>;
-    let pileup = 0;
 
     const takePicture = (request: SnapshotRequest) => {
         if (pendingPicture)
             return pendingPicture;
 
-        if (pileup > 200)
-            console.warn('Snapshot requests are piling up', pileup);
-
         if (device.interfaces.includes(ScryptedInterface.Camera)) {
-            pileup++;
             pendingPicture = device.takePicture({
                 picture: {
                     width: request.width,
@@ -38,16 +33,10 @@ export function createSnapshotHandler(device: ScryptedDevice & VideoCamera & Cam
             pendingPicture = Promise.reject(new Error('Camera does not provide native snapshots. Please install the Snapshot Plugin.'));
         }
 
-        pendingPicture = timeoutPromise(3000, pendingPicture)
-            .catch(e => {
-                if (e instanceof TimeoutError)
-                    recommendSnapshotPlugin(console, homekitPlugin.log, `${device.name} is offline or has slow snapshots. This will cause HomeKit to hang. Consider installing the Snapshot Plugin to keep HomeKit responsive. origin:/#/component/plugin/install/@scrypted/snapshot}`);
-                throw e;
-            })
+        pendingPicture
             .finally(() => {
-                pileup--;
                 pendingPicture = undefined;
-            });
+            })
 
         return pendingPicture;
     }
@@ -56,14 +45,13 @@ export function createSnapshotHandler(device: ScryptedDevice & VideoCamera & Cam
         leading: true,
         trailing: true,
     });
-
     function snapshotAll(request: SnapshotRequest) {
         for (const snapshotThrottle of homekitPlugin.snapshotThrottles.values()) {
             snapshotThrottle(request);
         }
     }
 
-    homekitPlugin.snapshotThrottles.set(device.id, throttledTakePicture);
+    homekitPlugin.snapshotThrottles.set(device.id, takePicture);
 
     async function handleSnapshotRequest(request: SnapshotRequest, callback: SnapshotRequestCallback) {
         try {
@@ -83,6 +71,8 @@ export function createSnapshotHandler(device: ScryptedDevice & VideoCamera & Cam
             // fetch everything serially.
             // this call is not a bug, to force lodash to take a picture on the trailing edge,
             // throttle must be called twice.
+
+            // no longer necessary in accessory mode?
             snapshotAll(request);
             snapshotAll(request);
 
