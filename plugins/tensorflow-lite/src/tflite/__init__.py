69d2d88f504896c8f47ab6b1963762124d7cfd14
Koushik Dutta <koushd@gmail.com>
2023-01-04 10:06:01 -0800

    tensorflow-lite/coreml: refactor to break out a general python prediction plugin

--- plugins/tensorflow-lite/src/tflite/__init__.py
+++ plugins/tensorflow-lite/src/tflite/__init__.py
@@ -1,15 +1,11 @@
 from __future__ import annotations
-from typing_extensions import TypedDict
-from scrypted_sdk.types import ObjectDetectionModel, ObjectDetectionResult, ObjectsDetected, Setting
 import threading
-import io
 from .common import *
-from PIL import Image, ImageOps
+from PIL import Image
 from pycoral.adapters import detect
 from pycoral.adapters.common import input_size
 loaded_py_coral = False
 try:
-    from pycoral.utils.edgetpu import run_inference
     from pycoral.utils.edgetpu import list_edge_tpus
     from pycoral.utils.edgetpu import make_interpreter
     loaded_py_coral = True
@@ -20,34 +16,8 @@ except Exception as e:
 import tflite_runtime.interpreter as tflite
 import re
 import scrypted_sdk
-from typing import Any, List, Tuple
-from gi.repository import Gst
-import asyncio
-import time
-
-from detect import DetectionSession, DetectPlugin
-from collections import namedtuple
-
-Rectangle = namedtuple('Rectangle', 'xmin ymin xmax ymax')
-
-def intersect_area(a: Rectangle, b: Rectangle):  # returns None if rectangles don't intersect
-    dx = min(a.xmax, b.xmax) - max(a.xmin, b.xmin)
-    dy = min(a.ymax, b.ymax) - max(a.ymin, b.ymin)
-    if (dx>=0) and (dy>=0):
-        return dx*dy
-
-class QueuedSample(TypedDict):
-    gst_buffer: Any
-    eventId: str
-
-class TensorFlowLiteSession(DetectionSession):
-    image: Image.Image
-
-    def __init__(self, start_time: float) -> None:
-        super().__init__()
-        self.image = None
-        self.processed = 0
-        self.start_time = start_time
+from typing import Any, Tuple
+from predict import PredictPlugin
 
 def parse_label_contents(contents: str):
     lines = contents.splitlines()
@@ -61,55 +31,11 @@ def parse_label_contents(contents: str):
     return ret
 
 
-class RawImage:
-    jpegMediaObject: scrypted_sdk.MediaObject
-
-    def __init__(self, image: Image.Image):
-        self.image = image
-        self.jpegMediaObject = None
-
 MIME_TYPE = 'x-scrypted-tensorflow-lite/x-raw-image'
 
-def is_same_detection(d1: ObjectDetectionResult, d2: ObjectDetectionResult):
-    if d1['className'] != d2['className']:
-        return False, None
-    
-    bb1 = d1['boundingBox']
-    bb2 = d2['boundingBox']
-
-    r1 = Rectangle(bb1[0], bb1[1], bb1[0] + bb1[2], bb1[1] + bb1[3])
-    r2 = Rectangle(bb2[0], bb2[1], bb2[0] + bb2[2], bb2[1] + bb2[3])
-    ia = intersect_area(r1, r2)
-
-    if not ia:
-        return False, None
-
-    a1 = bb1[2] * bb1[3]
-    a2 = bb2[2] * bb2[3]
-
-    # if area intersect area is too small, these are different boxes
-    if ia / a1 < .4 and ia / a2 < .4:
-        return False, None
-
-    l = min(bb1[0], bb2[0])
-    t = min(bb1[1], bb2[1])
-    r = max(bb1[0] + bb1[2], bb2[0] + bb2[2])
-    b = max(bb1[1] + bb1[3], bb2[1] + bb2[3])
-
-    w = r - l
-    h = b - t
-
-    return True, (l, t, w, h)
-
-
-class TensorFlowLitePlugin(DetectPlugin, scrypted_sdk.BufferConverter, scrypted_sdk.Settings):
+class TensorFlowLitePlugin(PredictPlugin, scrypted_sdk.BufferConverter, scrypted_sdk.Settings):
     def __init__(self, nativeId: str | None = None):
-        super().__init__(nativeId=nativeId)
-
-        self.fromMimeType = MIME_TYPE
-        self.toMimeType = scrypted_sdk.ScryptedMimeTypes.MediaObject.value
-
-        self.crop = False
+        super().__init__(MIME_TYPE, nativeId=nativeId)
 
         labels_contents = scrypted_sdk.zip.open(
             'fs/coco_labels.txt').read().decode('utf8')
@@ -131,163 +57,16 @@ class TensorFlowLitePlugin(DetectPlugin, scrypted_sdk.BufferConverter, scrypted_
             self.interpreter = tflite.Interpreter(model_content=model)
         self.interpreter.allocate_tensors()
         self.mutex = threading.Lock()
-        self.sampleQueue = []
 
-        # periodic restart because there seems to be leaks in tflite or coral API.
-        loop = asyncio.get_event_loop()
-        loop.call_later(60 * 60, lambda: self.requestRestart())
-
-    async def getSettings(self) -> list[Setting]:
-        coral: Setting = {
-            'title': 'Detected Edge TPU',
-            'description': 'The device paths of the Coral Edge TPUs that will be used for detections.',
-            'value': self.edge_tpu_found,
-            'readonly': True,
-            'key': 'coral',
-        }
-
-        return [coral]
-    
-    async def putSetting(self, key: str, value: scrypted_sdk.SettingValue) -> None:
-        pass
-
-    async def createMedia(sekf, data: RawImage) -> scrypted_sdk.MediaObject:
-        mo = await scrypted_sdk.mediaManager.createMediaObject(data, MIME_TYPE)
-        return mo
-
-    def end_session(self, detection_session: TensorFlowLiteSession):
-        image = detection_session.image
-        if image:
-            detection_session.image = None
-            image.close()
-
-        dps = detection_session.processed / (time.time() - detection_session.start_time)
-        print("Detections per second %s" % dps)
-        return super().end_session(detection_session)
-
-    def invalidateMedia(self, detection_session: TensorFlowLiteSession, data: RawImage):
-        if not data:
-            return
-        image = data.image
-        data.image = None
-        if image:
-            if not detection_session.image:
-                detection_session.image = image
-            else:
-                image.close()
-        data.jpegMediaObject = None
-
-    async def convert(self, data: RawImage, fromMimeType: str, toMimeType: str, options: scrypted_sdk.BufferConvertorOptions = None) -> Any:
-        mo = data.jpegMediaObject
-        if not mo:
-            image = data.image
-            if not image:
-                raise Exception('tensorflow-lite data is no longer valid')
-
-            bio = io.BytesIO()
-            image.save(bio, format='JPEG')
-            jpegBytes = bio.getvalue()
-            mo = await scrypted_sdk.mediaManager.createMediaObject(jpegBytes, 'image/jpeg')
-            data.jpegMediaObject = mo
-        return mo
-
-    def requestRestart(self):
-        asyncio.ensure_future(scrypted_sdk.deviceManager.requestRestart())
-
-    async def getDetectionModel(self, settings: Any = None) -> ObjectDetectionModel:
+    # width, height, channels
+    def get_input_details(self) -> Tuple[int, int, int]:
         with self.mutex:
             _, height, width, channels = self.interpreter.get_input_details()[
                 0]['shape']
+            return height, width, channels
 
-        d: ObjectDetectionModel = {
-            'name': '@scrypted/tensorflow-lite',
-            'classes': list(self.labels.values()),
-            'inputSize': [int(width), int(height), int(channels)],
-        }
-
-        decoderSetting: Setting = {
-            'title': "Decoder",
-            'description': "The gstreamer element used to decode the stream",
-            'combobox': True,
-            'value': 'Default',
-            'placeholder': 'Default',
-            'key': 'decoder',
-            'choices': [
-                'Default',
-                'decodebin',
-                'parsebin ! vtdec_hw',
-                'parsebin ! h264parse ! nvh264dec',
-                'rtph264depay ! h264parse ! nvh264dec',
-            ],
-        }
-        allowList: Setting = {
-            'title': 'Allow List',
-            'description': 'The detection classes that will be reported. If none are specified, all detections will be reported.',
-            'choices': list(self.labels.values()),
-            'multiple': True,
-            'key': 'allowList',
-            'value': [
-                'person',
-                'dog',
-                'cat',
-                'car',
-                'truck',
-                'bus',
-                'motorcycle',
-            ],
-        }
-
-        d['settings'] = [
-            decoderSetting,
-            allowList
-        ]
-        return d
-
-    def create_detection_result(self, objs, size, allowList, convert_to_src_size=None) -> ObjectsDetected:
-        detections: List[ObjectDetectionResult] = []
-        detection_result: ObjectsDetected = {}
-        detection_result['detections'] = detections
-        detection_result['inputDimensions'] = size
-
-        for obj in objs:
-            className = self.labels.get(obj.id, obj.id)
-            if allowList and len(allowList) and className not in allowList:
-                continue
-            detection: ObjectDetectionResult = {}
-            detection['boundingBox'] = (
-                obj.bbox.xmin, obj.bbox.ymin, obj.bbox.xmax - obj.bbox.xmin, obj.bbox.ymax - obj.bbox.ymin)
-            detection['className'] = className
-            detection['score'] = obj.score
-            detections.append(detection)
-
-        if convert_to_src_size:
-            detections = detection_result['detections']
-            detection_result['detections'] = []
-            for detection in detections:
-                bb = detection['boundingBox']
-                x, y, valid = convert_to_src_size((bb[0], bb[1]), True)
-                x2, y2, valid2 = convert_to_src_size(
-                    (bb[0] + bb[2], bb[1] + bb[3]), True)
-                if not valid or not valid2:
-                    # print("filtering out", detection['className'])
-                    continue
-                detection['boundingBox'] = (x, y, x2 - x + 1, y2 - y + 1)
-                detection_result['detections'].append(detection)
-
-        # print(detection_result)
-        return detection_result
-
-    def run_detection_jpeg(self, detection_session: TensorFlowLiteSession, image_bytes: bytes, settings: Any) -> ObjectsDetected:
-        stream = io.BytesIO(image_bytes)
-        image = Image.open(stream)
-
-        detections, _ = self.run_detection_image(detection_session, image, settings, image.size)
-        return detections
-
-    def get_detection_input_size(self, src_size):
-        return (None, None)
-        with self.mutex:
-            return input_size(self.interpreter)
+    def get_input_size(self) -> Tuple[float, float]:
+        return input_size(self.interpreter)
 
     def detect_once(self, input: Image.Image, settings: Any, src_size, cvss):
         with self.mutex:
@@ -303,161 +82,3 @@ class TensorFlowLitePlugin(DetectPlugin, scrypted_sdk.BufferConverter, scrypted_
         allowList = settings.get('allowList', None) if settings else None
         ret = self.create_detection_result(objs, src_size, allowList, cvss)
         return ret
-
-    def run_detection_image(self, detection_session: TensorFlowLiteSession, image: Image.Image, settings: Any, src_size, convert_to_src_size: Any = None, multipass_crop: Tuple[float, float, float, float] = None):
-        (w, h) = input_size(self.interpreter)
-
-        (iw, ih) = image.size
-
-        # this a single pass or the second pass. detect once and return results.
-        if multipass_crop:
-            (l, t, dx, dy) = multipass_crop
-
-            test = image.crop((l, t, l + dx, t + dy))
-
-            # find center
-            cx = l + dx / 2
-            cy = t + dy / 2
-
-            # fix aspect ratio on box
-            if dx / w > dy / h:
-                dy = dx / w * h
-            else:
-                dx = dy / h * w
-
-            # crop size to fit input size
-            if dx < w:
-                dx = w
-            if dy < h:
-                dy = h
-            
-            l = cx - dx / 2
-            t = cy - dy / 2
-            if l < 0:
-                l = 0
-            if t < 0:
-                t = 0
-            if l + dx > iw:
-                l = iw - dx
-            if t + dy > ih:
-                t = ih - dy
-            crop_box = (l, t, l + dx, t + dy)
-            input = image.crop(crop_box)
-            (cw, ch) = input.size
-            if cw != w or h != ch:
-                input = input.resize((w, h), Image.ANTIALIAS)
-
-            def cvss(point, normalize=False):
-                unscaled = ((point[0] / w) * cw + l, (point[1] / h) * ch + t)
-                converted = convert_to_src_size(unscaled, normalize) if convert_to_src_size else (unscaled[0], unscaled[1], True)
-                return converted
-
-            ret = self.detect_once(input, settings, src_size, cvss)
-            detection_session.processed = detection_session.processed + 1
-            return ret, RawImage(image)
-        
-        ws = w / iw
-        hs = h / ih
-        s = max(ws, hs)
-        if ws == 1 and hs == 1:
-            scaled = image
-        else:
-            scaled = image.resize((int(round(s * iw)), int(round(s * ih))), Image.ANTIALIAS)
-
-        first = scaled.crop((0, 0, w, h))
-        (sx, sy) = scaled.size
-        ow = sx - w
-        oh = sy - h
-        second = scaled.crop((ow, oh, ow + w, oh + h))
-
-        def cvss1(point, normalize=False):
-            unscaled = (point[0] / s, point[1] / s)
-            converted = convert_to_src_size(unscaled, normalize) if convert_to_src_size else (unscaled[0], unscaled[1], True)
-            return converted
-        def cvss2(point, normalize=False):
-            unscaled = ((point[0] + ow) / s, (point[1] + oh) / s)
-            converted = convert_to_src_size(unscaled, normalize) if convert_to_src_size else (unscaled[0], unscaled[1], True)
-            return converted
-     
-        ret1 = self.detect_once(first, settings, src_size, cvss1)
-        detection_session.processed = detection_session.processed + 1
-        ret2 = self.detect_once(second, settings, src_size, cvss2)
-        detection_session.processed = detection_session.processed + 1
-        r1Detections = list(ret1['detections'])
-        r2Detections = list(ret2['detections'])
-
-        ret = ret1
-        ret['detections'] = ret1['detections'] + ret2['detections']
-
-        if not len(ret['detections']):
-            return ret, RawImage(image)
-        
-
-        detections: List[ObjectDetectionResult]
-
-        def dedupe_detections():
-            nonlocal detections
-            detections = []
-            while len(ret['detections']):
-                d = ret['detections'].pop()
-                found = False
-                for c in detections:
-                    same, box = is_same_detection(d, c)
-                    if same:
-                        # encompass this box and score
-                        d['boundingBox'] = box
-                        d['score'] = max(d['score'], c['score'])
-                        # remove from current detections list
-                        detections = list(filter(lambda r: r != c, detections))
-                        # run dedupe again with this new larger item
-                        ret['detections'].append(d)
-                        found = True
-                        break
-
-                if not found:
-                    detections.append(d)
-
-        dedupe_detections()
-
-        ret['detections'] = detections
-        return ret, RawImage(image)
-
-    def run_detection_crop(self, detection_session: DetectionSession, sample: RawImage, settings: Any, src_size, convert_to_src_size, bounding_box: Tuple[float, float, float, float]) -> ObjectsDetected:
-        (ret, _) = self.run_detection_image(detection_session, sample.image, settings, src_size, convert_to_src_size, bounding_box)
-        return ret
-
-    def run_detection_gstsample(self, detection_session: TensorFlowLiteSession, gstsample, settings: Any, src_size, convert_to_src_size) -> Tuple[ObjectsDetected, Image.Image]:
-        # todo reenable this if detection images aren't needed.
-        if False and loaded_py_coral:
-            with self.mutex:
-                gst_buffer = gstsample.get_buffer()
-                run_inference(self.interpreter, gst_buffer)
-                objs = detect.get_objects(
-                    self.interpreter, score_threshold=score_threshold)
-        else:
-            caps = gstsample.get_caps()
-            # can't trust the width value, compute the stride
-            height = caps.get_structure(0).get_value('height')
-            width = caps.get_structure(0).get_value('width')
-            gst_buffer = gstsample.get_buffer()
-            result, info = gst_buffer.map(Gst.MapFlags.READ)
-            if not result:
-                return
-            try:
-                image = detection_session.image
-                detection_session.image = None
-
-                if image and (image.width != width or image.height != height):
-                    image.close()
-                    image = None
-                if image:
-                    image.frombytes(bytes(info.data))
-                else:
-                    image = Image.frombuffer('RGB', (width, height), bytes(info.data))
-            finally:
-                gst_buffer.unmap(info)
-
-        return self.run_detection_image(detection_session, image, settings, src_size, convert_to_src_size)
-
-    def create_detection_session(self):
-        return TensorFlowLiteSession(start_time=time.time())
