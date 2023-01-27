1a0777a20993edaed55d0b2b3d5097e8a8ef861b
Koushik Dutta <koushd@gmail.com>
2023-01-26 21:05:40 -0800

    sdk: add changelog generation

--- sdk/bin/scrypted-package-json.js
+++ sdk/bin/scrypted-package-json.js
@@ -2,17 +2,18 @@
 const fs = require('fs');
 
 const pkg = JSON.parse(fs.readFileSync('package.json'));
-pkg.scripts = {
+pkg.scripts = Object.assign({
     "scrypted-setup-project": "scrypted-setup-project",
     "prescrypted-setup-project": "scrypted-package-json",
     "build": "scrypted-webpack",
+    "preprepublishOnly": "scrypted-changelog",
     "prepublishOnly": "NODE_ENV=production scrypted-webpack",
     "prescrypted-vscode-launch": "scrypted-webpack",
     "scrypted-vscode-launch": "scrypted-deploy-debug",
     "scrypted-deploy-debug": "scrypted-deploy-debug",
     "scrypted-debug": "scrypted-debug",
     "scrypted-deploy": "scrypted-deploy",
-    "scrypted-readme": "scrypted-readme",
+    "scrypted-changelog": "scrypted-changelog",
     "scrypted-package-json": "scrypted-package-json",
-};
+}, pkg.scripts);
 fs.writeFileSync('package.json', JSON.stringify(pkg, null, 3) + '\n');
