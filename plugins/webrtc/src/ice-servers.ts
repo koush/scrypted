91ae143d9667473cd281bf81208504f0bc159242
Koushik Dutta <koushd@gmail.com>
2023-02-15 12:27:54 -0800

    webrtc: minor sdp tweaks

--- plugins/webrtc/src/ice-servers.ts
+++ plugins/webrtc/src/ice-servers.ts
@@ -25,11 +25,17 @@ export const stunIceServers = [
     googleStunServer,
 ];
 
-function toWerift(s: typeof turnServer) {
+function toWerift(s: typeof turnServer | RTCIceServer) {
     return {
-        urls: s.urls[0],
+        urls: typeof s.urls === 'string' ? s.urls : s.urls[0],
         username: s.username,
         credential: s.credential,
+    }
+}
+
+export function toWeriftConfiguration(configuration: RTCConfiguration) {
+    return {
+        iceServers: configuration.iceServers.map(toWerift),
     }
 }
 
