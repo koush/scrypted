20924915a1eb3dd5c3d2ce53dd2151e15b04c264
Alex Leeds <alexleeds@hotmail.com>
2023-02-23 09:19:18 -0500

    Add Support for Ring Contact Sensors

--- plugins/ring/src/ring-client-api.ts
+++ plugins/ring/src/ring-client-api.ts
@@ -9,4 +9,6 @@ export { BasicPeerConnection } from '@koush/ring-client-api/api/streaming/peer-c
 export { SimpleWebRtcSession } from '@koush/ring-client-api/api/streaming/simple-webrtc-session';
 export { StreamingSession } from '@koush/ring-client-api/api/streaming/streaming-session';
 export { generateUuid } from '@koush/ring-client-api/api/util';
+export { RingDeviceType, RingDeviceData } from '@koush/ring-client-api/api/ring-types';
+export { RingDevice } from '@koush/ring-client-api/api/ring-device';
 export * as rxjs from '@koush/ring-client-api/node_modules/rxjs';
