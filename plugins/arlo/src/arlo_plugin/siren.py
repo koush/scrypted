2d559db44387268ed0453dc07ce241139df93ffa
Brett Jia <dev.bjia56@gmail.com>
2023-03-18 12:15:13 -0400

    functional basestation siren

--- plugins/arlo/src/arlo_plugin/siren.py
+++ plugins/arlo/src/arlo_plugin/siren.py
@@ -1,4 +1,3 @@
-from scrypted_sdk import ScryptedDeviceBase
 from scrypted_sdk.types import OnOff, ScryptedInterface
 
 from .device_base import ArloDeviceBase
@@ -8,3 +7,11 @@ class ArloSiren(ArloDeviceBase, OnOff):
 
     def get_applicable_interfaces(self) -> list:
         return [ScryptedInterface.OnOff.value]
+
+    async def turnOn(self) -> None:
+        self.logger.info("Turning on")
+        self.provider.arlo.SirenOn(self.arlo_device)
+
+    async def turnOff(self) -> None:
+        self.logger.info("Turning off")
+        self.provider.arlo.SirenOff(self.arlo_device)
