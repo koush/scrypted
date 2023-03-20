effe76f2516deebd53887ac630ceba694b4deb60
Koushik Dutta <koush@koushikdutta.com>
2023-03-20 13:30:10 -0700

    docker: update massive nvidia build

--- docker/docker-build-nvidia.sh
+++ docker/docker-build-nvidia.sh
@@ -1,17 +1,3 @@
-./template/generate-dockerfile.sh
+./docker-build.sh
 
-set -x
-
-NODE_VERSION=18
-BUILDPACK_DEPS_BASE=bullseye
-FLAVOR=full
-BASE=$NODE_VERSION-$BUILDPACK_DEPS_BASE-$FLAVOR
-echo $BASE
-SUPERVISOR=.nvidia
-SUPERVISOR_BASE=$BASE$SUPERVISOR
-
-docker build -t koush/scrypted-common:$BASE -f Dockerfile.$FLAVOR \
-    --build-arg NODE_VERSION=$NODE_VERSION --build-arg BUILDPACK_DEPS_BASE=$BUILDPACK_DEPS_BASE . && \
-\
-docker build -t koush/scrypted:$SUPERVISOR_BASE -f Dockerfile$SUPERVISOR \
-    --build-arg BASE=$BASE .
+docker build -t koush/scrypted:18-bullseye-full.nvidia -f Dockerfile.nvidia
