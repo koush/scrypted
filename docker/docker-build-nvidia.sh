a51754b0e3279ddb7a17325151a8325d17570979
Koushik Dutta <koush@koushikdutta.com>
2023-03-20 08:42:28 -0700

    docker: add tensorflow nvidia support

---
+++ docker/docker-build-nvidia.sh
@@ -0,0 +1,17 @@
+./template/generate-dockerfile.sh
+
+set -x
+
+NODE_VERSION=18
+BUILDPACK_DEPS_BASE=bullseye
+FLAVOR=full
+BASE=$NODE_VERSION-$BUILDPACK_DEPS_BASE-$FLAVOR
+echo $BASE
+SUPERVISOR=.nvidia
+SUPERVISOR_BASE=$BASE$SUPERVISOR
+
+docker build -t koush/scrypted-common:$BASE -f Dockerfile.$FLAVOR \
+    --build-arg NODE_VERSION=$NODE_VERSION --build-arg BUILDPACK_DEPS_BASE=$BUILDPACK_DEPS_BASE . && \
+\
+docker build -t koush/scrypted:$SUPERVISOR_BASE -f Dockerfile$SUPERVISOR \
+    --build-arg BASE=$BASE .
