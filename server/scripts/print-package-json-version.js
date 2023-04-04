516f2a2a7b5cc4e77a8923583deae264bbb7825b
Koushik Dutta <koushd@gmail.com>
2023-04-04 10:14:31 -0700

    server: fetch version from package registry

--- server/scripts/print-package-json-version.js
+++ server/scripts/print-package-json-version.js
@@ -1,2 +1,9 @@
-const packageJson = require('../package.json');
-console.log(packageJson.version);
+async function main() {
+    const response = await fetch('https://registry.npmjs.org/@scrypted/server');
+    const json = await response.json();
+    console.log(json['dist-tags'].latest);
+    // const packageJson = require('../package.json');
+    // console.log(packageJson.version);
+}
+
+main();
