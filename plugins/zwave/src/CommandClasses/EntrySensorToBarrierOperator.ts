013e04cfd9272a768ef30e9626d80a97c0d600fa
Mike <mike@recut.com>
2023-04-11 17:03:22 -0700

    Fixing sensor mapping to position

--- plugins/zwave/src/CommandClasses/EntrySensorToBarrierOperator.ts
+++ plugins/zwave/src/CommandClasses/EntrySensorToBarrierOperator.ts
@@ -1,25 +1,19 @@
 import { EntrySensor } from "@scrypted/sdk";
 import type { ValueID } from "@zwave-js/core";
-import { BarrierState } from "zwave-js";
 import { ZwaveDeviceBase } from "./ZwaveDeviceBase";
 
 export class EntrySensorToBarriorOperator extends ZwaveDeviceBase implements EntrySensor {
     static updateState(zwaveDevice: ZwaveDeviceBase, valueId: ValueID) {
-        let currentValue: BarrierState
+        let currentValue: number;
         currentValue = zwaveDevice.getValue(valueId);
 
         switch (currentValue) {
-            case BarrierState.Closed:
+            case 0:
                 zwaveDevice.entryOpen = false;
                 break;
-            case BarrierState.Closing:
-            case BarrierState.Opening:
-            case BarrierState.Open:
-            case BarrierState.Stopped:
+            case 100:
                 zwaveDevice.entryOpen = true;
                 break;
-            default:
-                zwaveDevice.entryOpen = false;
         }
     }
 }
