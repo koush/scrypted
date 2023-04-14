7dd3d71ebd28bad46f9154a64bda8519393b1658
Koushik Dutta <koushd@gmail.com>
2023-04-14 13:36:56 -0700

    videoanalysis: remove problematic ffmpeg video generator

--- plugins/objectdetector/src/main.ts
+++ plugins/objectdetector/src/main.ts
@@ -5,7 +5,7 @@ import { StorageSettings } from '@scrypted/sdk/storage-settings';
 import crypto from 'crypto';
 import { AutoenableMixinProvider } from "../../../common/src/autoenable-mixin-provider";
 import { SettingsMixinDeviceBase } from "../../../common/src/settings-mixin";
-import { FFmpegVideoFrameGenerator, sharpLib } from './ffmpeg-videoframes';
+// import { FFmpegVideoFrameGenerator, sharpLib } from './ffmpeg-videoframes';
 import { serverSupportsMixinEventMasking } from './server-version';
 import { getAllDevices, safeParseJson } from './util';
 
@@ -952,25 +952,25 @@ class ObjectDetectionPlugin extends AutoenableMixinProvider implements Settings,
   constructor(nativeId?: ScryptedNativeId) {
     super(nativeId);
 
-    process.nextTick(() => {
-      sdk.deviceManager.onDevicesChanged({
-        devices: [
-          {
-            name: 'FFmpeg Frame Generator',
-            type: ScryptedDeviceType.Builtin,
-            interfaces: sharpLib ? [
-              ScryptedInterface.VideoFrameGenerator,
-            ] : [],
-            nativeId: 'ffmpeg',
-          }
-        ]
-      })
-    })
+    // process.nextTick(() => {
+    //   sdk.deviceManager.onDevicesChanged({
+    //     devices: [
+    //       {
+    //         name: 'FFmpeg Frame Generator',
+    //         type: ScryptedDeviceType.Builtin,
+    //         interfaces: sharpLib ? [
+    //           ScryptedInterface.VideoFrameGenerator,
+    //         ] : [],
+    //         nativeId: 'ffmpeg',
+    //       }
+    //     ]
+    //   })
+    // })
   }
 
   async getDevice(nativeId: string): Promise<any> {
-    if (nativeId === 'ffmpeg')
-      return new FFmpegVideoFrameGenerator('ffmpeg');
+    // if (nativeId === 'ffmpeg')
+    //   return new FFmpegVideoFrameGenerator('ffmpeg');
   }
 
   async releaseDevice(id: string, nativeId: string): Promise<void> {
