014009cea9b3b9e195b5b63b63aec8567c6ce8fb
Brett Jia <dev.bjia56@gmail.com>
2023-08-27 08:07:12 -0400

    core: use webpack bundled map marker

--- plugins/core/ui/src/interfaces/sensors/PositionSensor.vue
+++ plugins/core/ui/src/interfaces/sensors/PositionSensor.vue
@@ -13,16 +13,23 @@
     }"
   >
     <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-    <l-marker :lat-lng="position" :icon="icon"></l-marker>
+    <l-marker :lat-lng="position"></l-marker>
     <l-control-attribution position="bottomright" :prefix="prefix"></l-control-attribution>
   </l-map>
 </template>
 
 <script>
-import { latLng, icon } from "leaflet";
+import { latLng, Icon } from "leaflet";
 import { LMap, LTileLayer, LMarker, LControlAttribution } from "vue2-leaflet";
 import 'leaflet/dist/leaflet.css';
 import RPCInterface from "../RPCInterface.vue";
+
+delete Icon.Default.prototype._getIconUrl;
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
 
 export default {
   mixins: [RPCInterface],
@@ -37,10 +44,6 @@ export default {
       url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
       prefix: '<a target="blank" href="https://leafletjs.com/">Leaflet</a>',
       attribution: '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-      icon: icon({
-        iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
-        shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
-      }),
     };
   },
   computed: {
