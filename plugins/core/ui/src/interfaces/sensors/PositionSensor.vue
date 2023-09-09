4c11def52bc4c04bc330b84186023c8265fb3c15
Brett Jia <dev.bjia56@gmail.com>
2023-09-09 12:57:15 -0400

    core: use webpack bundled map marker (#1049)
    * core: use webpack bundled map marker
    
    * document source of marker icon workaround
    
    * disable touch zoom

--- plugins/core/ui/src/interfaces/sensors/PositionSensor.vue
+++ plugins/core/ui/src/interfaces/sensors/PositionSensor.vue
@@ -10,19 +10,28 @@
       doubleClickZoom: false,
       boxZoom: false,
       scrollWheelZoom: false,
+      touchZoom: false,
     }"
   >
     <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-    <l-marker :lat-lng="position" :icon="icon"></l-marker>
+    <l-marker :lat-lng="position"></l-marker>
     <l-control-attribution position="bottomright" :prefix="prefix"></l-control-attribution>
   </l-map>
 </template>
 
 <script>
-import { latLng, icon } from "leaflet";
+import { latLng, Icon } from "leaflet";
 import { LMap, LTileLayer, LMarker, LControlAttribution } from "vue2-leaflet";
 import 'leaflet/dist/leaflet.css';
 import RPCInterface from "../RPCInterface.vue";
+
+// https://vue2-leaflet.netlify.app/quickstart/#marker-icons-are-missing
+delete Icon.Default.prototype._getIconUrl;
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
 
 export default {
   mixins: [RPCInterface],
@@ -37,10 +46,6 @@ export default {
       url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
       prefix: '<a target="blank" href="https://leafletjs.com/">Leaflet</a>',
       attribution: '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-      icon: icon({
-        iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
-        shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
-      }),
     };
   },
   computed: {
