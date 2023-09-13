b8439aaec31040c1a9999ba267b7a54483ec6338
Koushik Dutta <koushd@gmail.com>
2023-09-13 16:17:16 -0700

    server: add axios post shim

--- server/src/fetch-helpers.ts
+++ server/src/fetch-helpers.ts
@@ -6,6 +6,17 @@ export async function fetchJSON(url: string, init?: RequestInit) {
     return await (await fetch(url, init)).json();
 }
 
+export async function fetchPostJSON(url: string, body: any, init?: RequestInit) {
+    init ||= {};
+    init.body = JSON.stringify(body);
+    init.method = 'POST';
+    init.headers = {
+        ...init.headers,
+        'Content-Type': 'application/json',
+    };
+    return await (await fetch(url, init)).json();
+}
+
 export async function fetchBuffer(url: string, init?: RequestInit) {
     return Buffer.from(await (await fetch(url, init)).arrayBuffer());
 }
