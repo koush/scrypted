805f471ff9accb7118d619d5d5bd51998fefe963
Koushik Dutta <koushd@gmail.com>
2023-12-21 00:10:43 -0800

    wyze: add audio support

--- plugins/wyze/src/main.py
+++ plugins/wyze/src/main.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import Any, Coroutine, List, Dict, Callable, Iterator
+from typing import Any, Coroutine, List, Dict, Callable, Iterator, MutableSet
 import scrypted_sdk
 import asyncio
 import urllib.request
@@ -13,6 +13,12 @@ import json
 import threading
 import queue
 import traceback
+from ctypes import c_int
+import concurrent.futures
+import subprocess
+import base64
+import struct
+
 from wyzecam.tutk.tutk import (
     FRAME_SIZE_1080P,
     FRAME_SIZE_360P,
@@ -36,10 +42,52 @@ os.environ["TUTK_PROJECT_ROOT"] = os.path.join(
 )
 sdkKey = "AQAAAIZ44fijz5pURQiNw4xpEfV9ZysFH8LYBPDxiONQlbLKaDeb7n26TSOPSGHftbRVo25k3uz5of06iGNB4pSfmvsCvm/tTlmML6HKS0vVxZnzEuK95TPGEGt+aE15m6fjtRXQKnUav59VSRHwRj9Z1Kjm1ClfkSPUF5NfUvsb3IAbai0WlzZE1yYCtks7NFRMbTXUMq3bFtNhEERD/7oc504b"
 
+toThreadExecutor = concurrent.futures.ThreadPoolExecutor(
+    max_workers=2, thread_name_prefix="image"
+)
+
+codecMap = {
+    "mulaw": "PCMU",
+    "alaw": "PCMA",
+    "s16be": "L16",
+    "opus": "OPUS",
+    "aac": "MP4A-LATM",
+}
+
+
+async def to_thread(f):
+    loop = asyncio.get_running_loop()
+    return await loop.run_in_executor(toThreadExecutor, f)
+
+
+class CodecInfo:
+    videoCodec: str
+    videoCodecInfo: tuple[bytes, bytes]
+    audioCodec: str
+    audioSampleRate: int
+
+    def __init__(
+        self,
+        videoCodec: str,
+        videoCodecInfo: tuple[bytes, bytes],
+        audioCodec: str,
+        audioSampleRate: int,
+    ) -> None:
+        self.videoCodec = videoCodec
+        self.videoCodecInfo = videoCodecInfo
+        self.audioCodec = audioCodec
+        self.audioSampleRate = audioSampleRate
+
 
 class WyzeCamera(scrypted_sdk.ScryptedDeviceBase, VideoCamera):
     camera: wyzecam.WyzeCamera
     plugin: WyzePlugin
+    streams: MutableSet[wyzecam.WyzeIOTCSession]
+    activeStream: wyzecam.WyzeIOTCSession
+    audioQueues: MutableSet[queue.Queue[tuple[bytes, Any]]]
+
+    main: CodecInfo
+    sub: CodecInfo
 
     def __init__(
         self, nativeId: str | None, plugin: WyzePlugin, camera: wyzecam.WyzeCamera
@@ -47,9 +95,23 @@ class WyzeCamera(scrypted_sdk.ScryptedDeviceBase, VideoCamera):
         super().__init__(nativeId=nativeId)
         self.plugin = plugin
         self.camera = camera
+        self.streams = set()
+        self.activeStream = None
+        self.audioQueues = set()
+        self.main = None
+        self.sub = None
 
         self.mainServer = asyncio.ensure_future(self.ensureServer(self.handleClientHD))
         self.subServer = asyncio.ensure_future(self.ensureServer(self.handleClientSD))
+        self.audioServer = asyncio.ensure_future(
+            self.ensureServer(self.handleAudioClient)
+        )
+        self.rfcServer = asyncio.ensure_future(
+            self.ensureServer(self.handleMainRfcClient)
+        )
+        self.rfcSubServer = asyncio.ensure_future(
+            self.ensureServer(self.handleSubRfcClient)
+        )
 
     async def handleClientHD(
         self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter
@@ -72,29 +134,135 @@ class WyzeCamera(scrypted_sdk.ScryptedDeviceBase, VideoCamera):
         return await self.handleClient(
             account,
             FRAME_SIZE_360P,
-            FRAME_SIZE_360P,
+            BITRATE_360P,
             reader,
             writer,
         )
 
-    async def handleAudioClient(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
-        return await self.handleClient(
-            self.plugin.account.model_copy(),
-            FRAME_SIZE_1080P,
-            BITRATE_HD,
-            reader,
-            writer,
-            session_reader=lambda sess: sess.recv_audio_frames()
-        )
+    def receiveAudioData(self):
+        q: queue.Queue[tuple[bytes, Any]] = queue.Queue()
+        self.audioQueues.add(q)
+        try:
+            while True:
+                b, info = q.get()
+                if not b:
+                    return
+                yield b, info
+        finally:
+            self.audioQueues.remove(q)
 
-    async def handleClient(
+    async def handleMainRfcClient(
+        self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter
+    ):
+        return await self.handleRfcClient(False, reader, writer)
+
+    async def handleSubRfcClient(
+        self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter
+    ):
+        return await self.handleRfcClient(True, reader, writer)
+
+    async def handleRfcClient(
         self,
-        account: wyzecam.WyzeAccount,
-        frameSize,
-        bitrate,
+        substream: bool,
         reader: asyncio.StreamReader,
         writer: asyncio.StreamWriter,
-        session_reader: Callable[[wyzecam.WyzeIOTCSession], Iterator[tuple[bytes | None, Any]]] = None,
+    ):
+        info = self.sub if substream else self.main
+
+        port = await self.subServer if substream else await self.mainServer
+        audioPort = await self.audioServer
+
+        class Protocol:
+            def __init__(self, pt: int) -> None:
+                self.pt = pt
+
+            def connection_made(self, transport):
+                self.transport = transport
+
+            def datagram_received(self, data, addr):
+                l = len(data)
+                len_data = struct.pack(">H", l)
+                writer.write(len_data)
+                writer.write(data)
+
+        ffmpeg = await scrypted_sdk.mediaManager.getFFmpegPath()
+        loop = asyncio.get_event_loop()
+
+        vt, vp = await loop.create_datagram_endpoint(
+            lambda: Protocol(96), local_addr=("127.0.0.1", 0)
+        )
+        vhost, vport = vt._sock.getsockname()
+
+        vprocess = subprocess.Popen(
+            [
+                ffmpeg,
+                "-analyzeduration",
+                "0",
+                "-probesize",
+                "100k",
+                "-f",
+                "h264",
+                "-i",
+                f"tcp://127.0.0.1:{port}",
+                "-vcodec",
+                "copy",
+                "-an",
+                "-f",
+                "rtp",
+                "-payload_type",
+                "96",
+                f"rtp://127.0.0.1:{vport}?pkt_size=1300",
+            ]
+        )
+
+        at, ap = await loop.create_datagram_endpoint(
+            lambda: Protocol(97), local_addr=("127.0.0.1", 0)
+        )
+
+        ahost, aport = at._sock.getsockname()
+
+        aprocess = subprocess.Popen(
+            [
+                ffmpeg,
+                "-analyzeduration",
+                "0",
+                "-probesize",
+                "1024",
+                "-f",
+                info.audioCodec,
+                "-ar",
+                f"{info.audioSampleRate}",
+                "-i",
+                f"tcp://127.0.0.1:{audioPort}",
+                "-acodec",
+                "copy",
+                "-vn",
+                "-f",
+                "rtp",
+                "-payload_type",
+                "97",
+                f"rtp://127.0.0.1:{aport}?pkt_size=1300",
+            ]
+        )
+
+        try:
+            while True:
+                buffer = await reader.read()
+                if not len(buffer):
+                    return
+        except Exception as e:
+            traceback.print_exception(e)
+        finally:
+            self.print("rfc reader closed")
+
+            # aprocess.stdin.write("q\n")
+            aprocess.terminate()
+
+            # vprocess.stdin.write("q\n")
+            vprocess.terminate()
+
+    async def handleAudioClient(
+        self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter
     ):
         loop = asyncio.get_event_loop()
         closed = False
@@ -113,19 +281,93 @@ class WyzeCamera(scrypted_sdk.ScryptedDeviceBase, VideoCamera):
 
         def run():
             try:
-                with wyzecam.WyzeIOTCSession(
-                    self.plugin.wyze_iotc.tutk_platform_lib,
-                    account,
-                    self.camera,
-                    frame_size=frameSize,
-                    bitrate=bitrate,
-                ) as sess:
-                    reader = (session_reader and session_reader(sess)) or sess.recv_video_data()
-                    for frame, frame_info in reader:
+                for frame, frame_info in self.receiveAudioData():
+                    if closed:
+                        return
+                    q.put(frame)
+                    asyncio.run_coroutine_threadsafe(write(), loop=loop)
+
+            except Exception as e:
+                traceback.print_exception(e)
+            finally:
+                self.print("audio session closed")
+                q.put(None)
+
+        thread = threading.Thread(target=run)
+        thread.start()
+
+        try:
+            while True:
+                buffer = await reader.read()
+                if not len(buffer):
+                    return
+        except Exception as e:
+            traceback.print_exception(e)
+        finally:
+            self.print("audio reader closed")
+            closed = True
+
+    async def handleClient(
+        self,
+        account: wyzecam.WyzeAccount,
+        frameSize,
+        bitrate,
+        reader: asyncio.StreamReader,
+        writer: asyncio.StreamWriter,
+    ):
+        loop = asyncio.get_event_loop()
+        closed = False
+        q = queue.Queue()
+
+        async def write():
+            nonlocal closed
+            d = q.get()
+            if closed:
+                pass
+            if not d or closed:
+                closed = True
+                writer.close()
+            else:
+                writer.write(d)
+
+        s = wyzecam.WyzeIOTCSession(
+            self.plugin.wyze_iotc.tutk_platform_lib,
+            account,
+            self.camera,
+            frame_size=frameSize,
+            bitrate=bitrate,
+            # CONNECTING?
+            stream_state=c_int(2),
+        )
+
+        self.streams.add(s)
+        startedAudio = False
+        if not self.activeStream:
+            self.activeStream = s
+
+        def runAudio():
+            for frame, frame_info in s.recv_audio_data():
+                for q in self.audioQueues:
+                    q.put((frame, frame_info))
+
+        def checkStartAudio():
+            nonlocal startedAudio
+            if not startedAudio and self.activeStream == s:
+                startedAudio = True
+                thread = threading.Thread(target=runAudio)
+                thread.start()
+
+        def run():
+            try:
+                with s as sess:
+                    checkStartAudio()
+                    for frame, frame_info in sess.recv_video_data():
                         if closed:
                             return
                         q.put(frame)
                         asyncio.run_coroutine_threadsafe(write(), loop=loop)
+                        checkStartAudio()
+
             except Exception as e:
                 traceback.print_exception(e)
             finally:
@@ -143,23 +385,112 @@ class WyzeCamera(scrypted_sdk.ScryptedDeviceBase, VideoCamera):
         except Exception as e:
             traceback.print_exception(e)
         finally:
+            self.streams.remove(s)
+            if self.activeStream == s:
+                # promote new audio stream to active
+                self.activeStream = None
+                for next in self.streams:
+                    self.activeStream = next
+                    break
             self.print("reader closed")
             closed = True
 
-    async def ensureServer(self, cb):
+    async def ensureServer(self, cb) -> int:
         server = await asyncio.start_server(cb, "127.0.0.1", 0)
         sock = server.sockets[0]
         host, port = sock.getsockname()
         asyncio.ensure_future(server.serve_forever())
         return port
 
+    def probeCodec(self, account, frameSize, bitrate):
+        with wyzecam.WyzeIOTCSession(
+            self.plugin.wyze_iotc.tutk_platform_lib,
+            account,
+            self.camera,
+            frame_size=frameSize,
+            bitrate=bitrate,
+            # CONNECTING?
+            stream_state=c_int(2),
+        ) as sess:
+            audioCodec = sess.get_audio_codec()
+            for data, frame_info in sess.recv_video_data():
+                nals = data.split(b"\x00\x00\x00\x01")
+                sps = nals[1]
+                pps = nals[2]
+                return audioCodec + (sps, pps)
+
+    def probeMainCodec(self):
+        return self.probeCodec(
+            self.plugin.account.model_copy(),
+            FRAME_SIZE_1080P,
+            BITRATE_HD,
+        )
+
+    def probeSubCodec(self):
+        account = self.plugin.account.model_copy()
+        account.phone_id = account.phone_id[2:]
+        return self.probeCodec(
+            account,
+            FRAME_SIZE_360P,
+            BITRATE_360P,
+        )
+
     async def getVideoStream(
         self, options: RequestMediaStreamOptions = None
     ) -> Coroutine[Any, Any, MediaObject]:
         substream = options and options.get("id") == "substream"
+
+        if substream:
+            if not self.sub:
+                codec, sampleRate, sps, pps = await to_thread(self.probeSubCodec)
+                self.sub = CodecInfo("h264", (sps, pps), codec, sampleRate)
+            info = self.sub
+
+        if not substream:
+            if not self.main:
+                codec, sampleRate, sps, pps = await to_thread(self.probeMainCodec)
+                self.main = CodecInfo("h264", (sps, pps), codec, sampleRate)
+            info = self.main
+
         port = await self.subServer if substream else await self.mainServer
+        audioPort = await self.audioServer
+        rfcPort = await self.rfcSubServer if substream else await self.rfcServer
+
         msos = self.getVideoStreamOptionsInternal()
         mso = msos[1] if substream else msos[0]
+        mso["audio"]["sampleRate"] = info.audioSampleRate
+
+        if True:
+            sps = base64.b64encode(info.videoCodecInfo[0]).decode()
+            pps = base64.b64encode(info.videoCodecInfo[1]).decode()
+            audioCodecName = codecMap.get(info.audioCodec)
+            sdp = f"""v=0
+o=- 0 0 IN IP4 0.0.0.0
+s=No Name
+t=0 0
+m=video 0 RTP/AVP 96
+c=IN IP4 0.0.0.0
+a=rtpmap:96 H264/90000
+a=fmtp:96 packetization-mode=1; sprop-parameter-sets={sps},{pps}; profile-level-id=4D0029
+m=audio 0 RTP/AVP 97
+c=IN IP4 0.0.0.0
+b=AS:128
+a=rtpmap:97 {audioCodecName}/{info.audioSampleRate}/1
+"""
+            rfc = {
+                "url": f"tcp://127.0.0.1:{rfcPort}",
+                "sdp": sdp,
+                "mediaStreamOptions": mso,
+            }
+            jsonString = json.dumps(rfc)
+            mo = await scrypted_sdk.mediaManager.createMediaObject(
+                jsonString.encode(),
+                "x-scrypted/x-rfc4571",
+                {
+                    "sourceId": self.id,
+                },
+            )
+            return mo
 
         ffmpegInput: scrypted_sdk.FFmpegInput = {
             "container": "ffmpeg",
@@ -173,6 +504,14 @@ class WyzeCamera(scrypted_sdk.ScryptedDeviceBase, VideoCamera):
                 "h264",
                 "-i",
                 f"tcp://127.0.0.1:{port}",
+                "-f",
+                info.audioCodec,
+                "-ar",
+                f"{info.audioBitrate}",
+                "-ac",
+                "1",
+                "-i",
+                f"tcp://127.0.0.1:{audioPort}",
             ],
         }
         mo = await scrypted_sdk.mediaManager.createFFmpegMediaObject(
@@ -194,6 +533,7 @@ class WyzeCamera(scrypted_sdk.ScryptedDeviceBase, VideoCamera):
                     "width": 1920,
                     "height": 1080,
                 },
+                "audio": {},
             }
         )
         # not all wyze can substream, need to create an exhaustive list?
@@ -208,6 +548,7 @@ class WyzeCamera(scrypted_sdk.ScryptedDeviceBase, VideoCamera):
                     "width": 640,
                     "height": 360,
                 },
+                "audio": {},
             }
         )
         return ret
