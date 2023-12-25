3bcb94fc6b3eb78be17abdf192850b0c8a341a42
Koushik Dutta <koushd@gmail.com>
2023-12-25 12:03:14 -0800

    reolink: increase motion timeout

--- plugins/reolink/src/main.ts
+++ plugins/reolink/src/main.ts
@@ -30,7 +30,7 @@ class ReolinkCamera extends RtspSmartCamera implements Camera, Reboot, Intercom,
         motionTimeout: {
             group: 'Advanced',
             title: 'Motion Timeout',
-            defaultValue: 10,
+            defaultValue: 20,
             type: 'number',
         },
         hasObjectDetector: {
