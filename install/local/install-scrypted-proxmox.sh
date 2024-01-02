778f0b7ad1a6bcb005943e5a0df7c5496d880ba7
Koushik Dutta <koushd@gmail.com>
2024-01-02 14:03:17 -0800

    proxmox: update script

--- install/local/install-scrypted-proxmox.sh
+++ install/local/install-scrypted-proxmox.sh
@@ -1,7 +1,3 @@
-cd /tmp
-curl -O -L https://github.com/koush/scrypted/releases/download/v0.72.0/scrypted.tar.zst
-pct restore 10443 scrypted.tar.zst
-
 function readyn() {
     while true; do
         read -p "$1 (y/n) " yn
@@ -13,6 +9,48 @@ function readyn() {
     done
 }
 
+cd /tmp
+SCRYPTED_VERSION=v0.72.0
+SCRYPTED_TAR_ZST=scrypted-$SCRYPTED_VERSION.tar.zst
+if [ -z "$VMID" ]
+then
+    VMID=10443
+fi
+
+echo "Downloading scrypted container backup."
+if [ ! -f "$SCRYPTED_TAR_ZST" ]
+then
+    curl -O -L https://github.com/koush/scrypted/releases/download/$SCRYPTED_VERSION/scrypted.tar.zst
+    mv scrypted.tar.zst $SCRYPTED_TAR_ZST
+fi
+
+echo "Checking for existing container."
+pct config $VMID
+if [ "$?" == "0" ]
+then
+    echo ""
+    echo "Existing container $VMID found. Run this script with --force to overwrite the existing container."
+    echo "This will wipe all existing data. Clone the existing container to retain the data, then reassign the owner of the scrypted volume after installation is complete."
+    echo ""
+    echo "bash $0 --force"
+    echo ""
+fi
+
+pct restore $VMID $SCRYPTED_TAR_ZST $@
+
+if [ "$?" != "0" ]
+then
+    echo ""
+    echo "pct restore failed"
+    echo ""
+    echo "This may be caused by the server's 'local' storage not supporting containers."
+    echo "Try running this script again with a different storage device (local-lvm, local-zfs). For example:"
+    echo ""
+    echo "bash $0 --storage local-lvm"
+    echo ""
+    exit 1
+fi
+
 echo "Adding udev rule: /etc/udev/rules.d/65-scrypted.rules"
 readyn "Add udev rule for hardware acceleration? This may conflict with existing rules."
 if [ "$yn" == "y" ]
@@ -23,4 +61,6 @@ then
     udevadm control --reload-rules && udevadm trigger
 fi
 
-echo "Scrypted setup is complete and the container can be started."
+echo "Scrypted setup is complete and the container resources can be started."
+echo "Scrypted NVR users should provide at least 4 cores and 16GB RAM prior to starting."
+
