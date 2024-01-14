54193251ab1a8b8267dd2affe2f841673a1a8e80
Koushik Dutta <koushd@gmail.com>
2024-01-13 22:17:40 -0800

    server: bump minimum core version

--- server/src/runtime.ts
+++ server/src/runtime.ts
@@ -52,7 +52,7 @@ interface DeviceProxyPair {
     proxy: ScryptedDevice;
 }
 
-const MIN_SCRYPTED_CORE_VERSION = 'v0.1.147';
+const MIN_SCRYPTED_CORE_VERSION = 'v0.2.4';
 const PLUGIN_DEVICE_STATE_VERSION = 2;
 
 interface HttpPluginData {
