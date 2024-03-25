df705cb0e74e6e9f8e27716846570957a90138e6
Koushik Dutta <koushd@gmail.com>
2024-03-25 12:09:00 -0700

    server: rollback portable python to 3.10

--- server/bin/packaged-python.js
+++ server/bin/packaged-python.js
@@ -3,7 +3,7 @@ const { PortablePython } = require('py')
 const { once } = require('events');
 
 module.exports = {
-    version: '3.11',
+    version: '3.10',
 }
 
 async function pipInstall(python, pkg) {
