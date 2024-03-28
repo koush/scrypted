6aabd20e24eb5ec458ea1aa7a1c561ca6bb51ffa
Brett Jia <dev.bjia56@gmail.com>
2024-03-28 19:55:43 -0400

    modify annotation

--- server/python/plugin_remote.py
+++ server/python/plugin_remote.py
@@ -56,7 +56,7 @@ class DeviceProxy(object):
     def __init__(self, systemManager: SystemManager, id: str):
         self.systemManager = systemManager
         self.id = id
-        self.device: asyncio.Future[Any] = None
+        self.device: asyncio.Future[rpc.RpcProxy] = None
 
     def __getattr__(self, name):
         if name == 'id':
