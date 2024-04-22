9cc04936990c48fb0f8baf57a410935eeebcb6ae
Brett Jia <dev.bjia56@gmail.com>
2024-04-21 23:01:11 -0400

    checkpoint fork-based implementation

--- plugins/rknn/src/main.py
+++ plugins/rknn/src/main.py
@@ -1,4 +1,7 @@
-from rknn import RKNNPlugin
+from rknn import RKNNPlugin, RKNNPluginProxy
 
-def create_scrypted_plugin():
+def create_scrypted_plugin() -> RKNNPluginProxy:
+    return RKNNPluginProxy()
+
+async def fork() -> RKNNPlugin:
     return RKNNPlugin()
