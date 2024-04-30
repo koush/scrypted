758bf0621505c222682276c5806c5b663c93cad8
Long Zheng <long.zheng@gmail.com>
2024-04-30 20:22:25 +1000

    Fix comment

--- install/local/install-scrypted-dependencies-win.ps1
+++ install/local/install-scrypted-dependencies-win.ps1
@@ -47,7 +47,7 @@ catch (e) {
 const child_process = require('child_process');
 child_process.spawn('$($NPX_PATH_ESCAPED)', ['-y', 'scrypted', 'serve'], {
     stdio: 'inherit',
-    # allow spawning .cmd https://nodejs.org/en/blog/vulnerability/april-2024-security-releases-2
+    // allow spawning .cmd https://nodejs.org/en/blog/vulnerability/april-2024-security-releases-2
     shell: true,
 });
 "@
