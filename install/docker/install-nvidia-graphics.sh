1abda3b42569a43f6483a0a1f63a4a0aed7b065f
Koushik Dutta <koush@Koushik-MacBook.local>
2024-05-03 23:54:06 -0700

    docker: add both nvidia libs

--- install/docker/install-nvidia-graphics.sh
+++ install/docker/install-nvidia-graphics.sh
@@ -1,16 +1,13 @@
 if [ "$(uname -m)" = "x86_64" ]
 then
-    # uncomment to require cuda 12, but everything currently targets 11.
-    # stuff that targets 11 will work with 12 though. 
-    # export CUDA_VERSION=12-4
-    export CUDA_VERSION=11-8
     echo "Installing NVIDIA graphics packages."
     apt update -q \
         && apt install wget \
         && wget -qO /cuda-keyring.deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb \
         && dpkg -i /cuda-keyring.deb \
         && apt update -q \
-        && apt install -y cuda-nvcc-$CUDA_VERSION libcublas-$CUDA_VERSION libcudnn8 cuda-libraries-$CUDA_VERSION;
+        && apt install -y cuda-nvcc-11-8 libcublas-11-8 libcudnn8 cuda-libraries-11-8 \
+        && apt install -y cuda-nvcc-12-4 libcublas-12-4 libcudnn8 cuda-libraries-12-4;
     exit $?
 else
     echo "NVIDIA graphics will not be installed on this architecture."
