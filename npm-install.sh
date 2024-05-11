46113744b366b30535a22f00a38453ab970bfb9e
Koushik Dutta <koushd@gmail.com>
2024-05-11 10:07:18 -0700

    dev: fix setup script

--- npm-install.sh
+++ npm-install.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-echo 'if (!process.version.startsWith("v18")) throw new Error("Node 18 is required. Install Node Version Manager (nvm) for versioned node installations. See https://github.com/koush/scrypted/pull/498#issuecomment-1373854020")' | node
-if [ "$?" != 0 ]
-then
-    exit
-fi
-
 echo ######################################
 echo "Setting up popular plugins."
 echo "Additional will need npm install manually."
@@ -15,7 +9,7 @@ cd $(dirname $0)
 git submodule init
 git submodule update
 
-for directory in sdk common server packages/client packages/auth-fetch
+for directory in sdk server common packages/client packages/auth-fetch
 do
     echo "$directory > npm install"
     pushd $directory
