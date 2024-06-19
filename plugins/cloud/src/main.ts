3da49d47af96be108f846c9749a3d096e512a814
Koushik Dutta <koush@Koushik-MacBook.localdomain>
2024-06-19 08:37:20 -0700

    cloud: fix cloudflared exit

--- plugins/cloud/src/main.ts
+++ plugins/cloud/src/main.ts
@@ -951,13 +951,13 @@ class ScryptedCloud extends ScryptedDeviceBase implements OauthClient, Settings,
     }
 
     async startCloudflared() {
-        if (!this.storageSettings.values.cloudflareEnabled) {
-            this.console.log('cloudflared is disabled.');
-            return;
-        }
-
         while (true) {
             try {
+                if (!this.storageSettings.values.cloudflareEnabled) {
+                    this.console.log('cloudflared is disabled.');
+                    return;
+                }
+
                 this.console.log('starting cloudflared');
                 this.cloudflared = await backOff(async () => {
                     const pluginVolume = process.env.SCRYPTED_PLUGIN_VOLUME;
@@ -1057,10 +1057,11 @@ class ScryptedCloud extends ScryptedDeviceBase implements OauthClient, Settings,
                     maxDelay: 300000,
                 });
 
-                await once(this.cloudflared.child, 'exit');
-                throw new Error('cloudflared exited.');
+                await once(this.cloudflared.child, 'exit').catch(() => { });
+                // the successfully started cloudflared process may exit at some point, loop ad allow it to restart.
             }
             catch (e) {
+                // this error may be reached if the cloudflared backoff fails.
                 this.console.error('cloudflared error', e);
                 throw e;
             }
