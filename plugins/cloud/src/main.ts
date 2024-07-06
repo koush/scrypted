9dc5f2a063b07a5b9330c1cce6174e5a7b699fc3
Koushik Dutta <koushd@gmail.com>
2024-07-05 17:57:08 -0700

    cloud: prefer cloudflared address if available

--- plugins/cloud/src/main.ts
+++ plugins/cloud/src/main.ts
@@ -888,10 +888,9 @@ class ScryptedCloud extends ScryptedDeviceBase implements OauthClient, Settings,
         this.proxy.on('proxyRes', (res, req) => {
             res.headers['X-Scrypted-Cloud'] = req.headers['x-scrypted-cloud'];
             res.headers['X-Scrypted-Direct-Address'] = req.headers['x-scrypted-direct-address'];
-            let domain: string;
-            if (this.storageSettings.values.forwardingMode === 'Custom Domain' && this.storageSettings.values.hostname)
+            let domain = this.cloudflareTunnel;
+            if (!domain && this.storageSettings.values.forwardingMode === 'Custom Domain' && this.storageSettings.values.hostname)
                 domain = `https://${this.storageSettings.values.hostname}`;
-            domain ||= this.cloudflareTunnel;
             res.headers['X-Scrypted-Cloud-Address'] = domain;
             res.headers['Access-Control-Expose-Headers'] = 'X-Scrypted-Cloud, X-Scrypted-Direct-Address, X-Scrypted-Cloud-Address';
         });
