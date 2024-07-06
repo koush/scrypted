68516817aa3a1b9952429809de4ac091a9d79576
Koushik Dutta <koushd@gmail.com>
2024-07-06 00:08:43 -0700

    cloud: fix tunnel registration failure hang

--- plugins/cloud/src/main.ts
+++ plugins/cloud/src/main.ts
@@ -1019,6 +1019,10 @@ class ScryptedCloud extends ScryptedDeviceBase implements OauthClient, Settings,
 
                         const lines = string.split('\n');
                         for (const line of lines) {
+                            if (line.includes('Register tunnel error') && deferred.finished ) {
+                                this.console.warn('Cloudflare registration failed after tunnel started. The old tunnel may be invalid. Terminating.');
+                                cloudflareTunnel.child.kill();
+                            }
                             if (line.includes('hostname'))
                                 this.console.log(line);
                             const match = /config=(".*?}")/gm.exec(line)
