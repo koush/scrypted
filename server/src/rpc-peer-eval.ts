be217021a25fa3e695a17a545e4cd58e444339ab
Koushik Dutta <koushd@gmail.com>
2024-08-03 10:22:24 -0700

    rpc: remove usage of vm module, embed filename using sourceURL

--- server/src/rpc-peer-eval.ts
+++ server/src/rpc-peer-eval.ts
@@ -1,25 +1,20 @@
-import type { CompileFunctionOptions } from 'vm';
-import { RpcPeer } from "./rpc";
+import type { RpcPeer } from "./rpc";
 
-type CompileFunction = (code: string, params?: ReadonlyArray<string>, options?: CompileFunctionOptions) => Function;
+export interface CompileFunctionOptions {
+    filename?: string;
+}
 
 function compileFunction(code: string, params?: ReadonlyArray<string>, options?: CompileFunctionOptions): any {
     params = params || [];
-    const f = `(function(${params.join(',')}) {;${code};})`;
+    if (options?.filename)
+        code = `${code}\n//# sourceURL=${options.filename}\n`;
+    const f = `(function(${params.join(',')}) {;${code}\n;})`;
     return eval(f);
 }
 
 export function evalLocal<T>(peer: RpcPeer, script: string, filename?: string, coercedParams?: { [name: string]: any }): T {
     const params = Object.assign({}, peer.params, coercedParams);
-    let compile: CompileFunction;
-    try {
-        // prevent bundlers from trying to include non-existent vm module.
-        compile = module[`require`]('vm').compileFunction;
-    }
-    catch (e) {
-        compile = compileFunction;
-    }
-    const f = compile(script, Object.keys(params), {
+    const f = compileFunction(script, Object.keys(params), {
         filename,
     });
     const value = f(...Object.values(params));
