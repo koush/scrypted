a04d463e0f6629444d13880331ddb16d55fd0ca6
Koushik Dutta <koush@Koushik-MacBook.localdomain>
2024-08-04 12:17:30 -0700

    docker/proxmox: add/update intel gpu and npu scripts

--- install/docker/install-intel-graphics.sh
+++ install/docker/install-intel-graphics.sh
@@ -1,38 +1,43 @@
-if [ "$(uname -m)" = "x86_64" ]
+if [ "$(uname -m)" != "x86_64" ]
 then
-    # this script previvously apt install intel-media-va-driver-non-free, but that seems to no longer be necessary.
-
-    # the intel provided script is disabled since it does not work with the 6.8 kernel in Ubuntu 24.04 or Proxmox 8.2.
-    # manual installation of the Intel graphics stuff is required.
-
-    # echo "Installing Intel graphics packages."
-    # apt-get update && apt-get install -y gpg-agent &&
-    # rm -f /usr/share/keyrings/intel-graphics.gpg &&
-    # curl -L https://repositories.intel.com/graphics/intel-graphics.key | gpg --dearmor --yes --output /usr/share/keyrings/intel-graphics.gpg &&
-    # echo 'deb [arch=amd64,i386 signed-by=/usr/share/keyrings/intel-graphics.gpg] https://repositories.intel.com/graphics/ubuntu jammy arc' | tee  /etc/apt/sources.list.d/intel.gpu.jammy.list &&
-    # apt-get -y update &&
-    # apt-get -y install intel-opencl-icd &&
-    # apt-get -y dist-upgrade;
-
-    # manual installation
-    # https://github.com/intel/compute-runtime/releases/tag/24.13.29138.7
-
-    rm -rf /tmp/neo && mkdir -p /tmp/neo && cd /tmp/neo &&
-    apt-get install -y ocl-icd-libopencl1 &&
-    curl -O -L https://github.com/intel/intel-graphics-compiler/releases/download/igc-1.0.17193.4/intel-igc-core_1.0.17193.4_amd64.deb &&
-    curl -O -L https://github.com/intel/intel-graphics-compiler/releases/download/igc-1.0.17193.4/intel-igc-opencl_1.0.17193.4_amd64.deb &&
-    curl -O -L https://github.com/intel/compute-runtime/releases/download/24.26.30049.6/intel-level-zero-gpu-dbgsym_1.3.30049.6_amd64.ddeb &&
-    curl -O -L https://github.com/intel/compute-runtime/releases/download/24.26.30049.6/intel-level-zero-gpu_1.3.30049.6_amd64.deb &&
-    curl -O -L https://github.com/intel/compute-runtime/releases/download/24.26.30049.6/intel-opencl-icd-dbgsym_24.26.30049.6_amd64.ddeb &&
-    curl -O -L https://github.com/intel/compute-runtime/releases/download/24.26.30049.6/intel-opencl-icd_24.26.30049.6_amd64.deb &&
-    curl -O -L https://github.com/intel/compute-runtime/releases/download/24.26.30049.6/libigdgmm12_22.3.20_amd64.deb &&
-    dpkg -i *.deb &&
-    cd /tmp && rm -rf /tmp/neo &&
-    apt-get -y dist-upgrade;
-
-    exit $?
-else
     echo "Intel graphics will not be installed on this architecture."
+    exit 0
 fi
 
-exit 0
+# this script previvously apt install intel-media-va-driver-non-free, but that seems to no longer be necessary.
+
+# the intel provided script is disabled since it does not work with the 6.8 kernel in Ubuntu 24.04 or Proxmox 8.2.
+# manual installation of the Intel graphics stuff is required.
+
+# echo "Installing Intel graphics packages."
+# apt-get update && apt-get install -y gpg-agent &&
+# rm -f /usr/share/keyrings/intel-graphics.gpg &&
+# curl -L https://repositories.intel.com/graphics/intel-graphics.key | gpg --dearmor --yes --output /usr/share/keyrings/intel-graphics.gpg &&
+# echo 'deb [arch=amd64,i386 signed-by=/usr/share/keyrings/intel-graphics.gpg] https://repositories.intel.com/graphics/ubuntu jammy arc' | tee  /etc/apt/sources.list.d/intel.gpu.jammy.list &&
+# apt-get -y update &&
+# apt-get -y install intel-opencl-icd &&
+# apt-get -y dist-upgrade;
+
+# manual installation
+# https://github.com/intel/compute-runtime/releases/tag/24.13.29138.7
+
+# no errors beyond this point
+set -e
+
+rm -rf /tmp/gpu && mkdir -p /tmp/gpu && cd /tmp/gpu
+
+apt-get install -y ocl-icd-libopencl1
+
+curl -O -L https://github.com/intel/intel-graphics-compiler/releases/download/igc-1.0.17193.4/intel-igc-core_1.0.17193.4_amd64.deb
+curl -O -L https://github.com/intel/intel-graphics-compiler/releases/download/igc-1.0.17193.4/intel-igc-opencl_1.0.17193.4_amd64.deb
+curl -O -L https://github.com/intel/compute-runtime/releases/download/24.26.30049.6/intel-level-zero-gpu-dbgsym_1.3.30049.6_amd64.ddeb
+curl -O -L https://github.com/intel/compute-runtime/releases/download/24.26.30049.6/intel-level-zero-gpu_1.3.30049.6_amd64.deb
+curl -O -L https://github.com/intel/compute-runtime/releases/download/24.26.30049.6/intel-opencl-icd-dbgsym_24.26.30049.6_amd64.ddeb
+curl -O -L https://github.com/intel/compute-runtime/releases/download/24.26.30049.6/intel-opencl-icd_24.26.30049.6_amd64.deb
+curl -O -L https://github.com/intel/compute-runtime/releases/download/24.26.30049.6/libigdgmm12_22.3.20_amd64.deb
+
+dpkg -i *.deb
+
+cd /tmp && rm -rf /tmp/gpu
+
+apt-get -y dist-upgrade
---
+++ install/docker/install-intel-npu.sh
@@ -0,0 +1,45 @@
+if [ "$(uname -m)" != "x86_64" ]
+then
+    echo "Intel NPU will not be installed on this architecture."
+    exit 0
+fi
+
+UBUNTU_22_04=$(lsb_release -r | grep "22.04")
+UBUNTU_24_04=$(lsb_release -r | grep "24.04")
+
+# needs either ubuntu 22.0.4 or 24.04
+if [ -z "$UBUNTU_22_04" ] && [ -z "$UBUNTU_24_04" ]
+then
+    echo "Intel NPU will not be installed. Ubuntu version could not be detected."
+    exit 0
+fi
+
+dpkg --purge --force-remove-reinstreq intel-driver-compiler-npu intel-level-zero-npu
+
+# no errors beyond this point
+set -e
+
+rm -rf /tmp/npu && mkdir -p /tmp/npu && cd /tmp/npu
+
+# different npu downloads for ubuntu versions
+if [ -n "$UBUNTU_22_04" ]
+then
+    curl -O -L https://github.com/intel/linux-npu-driver/releases/download/v1.5.1/intel-driver-compiler-npu_1.5.1.20240708-9842236399_ubuntu22.04_amd64.deb
+    curl -O -L https://github.com/intel/linux-npu-driver/releases/download/v1.5.1/intel-fw-npu_1.5.1.20240708-9842236399_ubuntu22.04_amd64.deb
+    curl -O -L https://github.com/intel/linux-npu-driver/releases/download/v1.5.1/intel-level-zero-npu_1.5.1.20240708-9842236399_ubuntu22.04_amd64.deb    
+else
+    curl -O -L https://github.com/intel/linux-npu-driver/releases/download/v1.5.1/intel-driver-compiler-npu_1.5.1.20240708-9842236399_ubuntu24.04_amd64.deb
+    curl -O -L https://github.com/intel/linux-npu-driver/releases/download/v1.5.1/intel-fw-npu_1.5.1.20240708-9842236399_ubuntu24.04_amd64.deb
+    curl -O -L https://github.com/intel/linux-npu-driver/releases/download/v1.5.1/intel-level-zero-npu_1.5.1.20240708-9842236399_ubuntu24.04_amd64.deb
+fi
+
+curl -O -L https://github.com/oneapi-src/level-zero/releases/download/v1.17.6/level-zero_1.17.6+u22.04_amd64.deb
+curl -O -L https://github.com/oneapi-src/level-zero/releases/download/v1.17.6/level-zero-devel_1.17.6+u22.04_amd64.deb
+
+apt -y update
+apt -y install libtbb12
+dpkg -i *.deb
+
+cd /tmp && rm -rf /tmp/npu
+
+apt-get -y dist-upgrade
