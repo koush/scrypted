e42eed318189345db62aeb031308a092c6dd0e3b
Brett Jia <dev.bjia56@gmail.com>
2024-08-05 16:45:44 -0400

    server: allow plugins to customize python installs

--- server/bin/packaged-python.d.ts
+++ server/bin/packaged-python.d.ts
@@ -1,2 +1,2 @@
 export declare declare const version: string;
-export declare async function installScryptedServerRequirements(version?: string, dest?: string): Promise<string>;
+export declare async function installScryptedServerRequirements(version?: string, dest?: string, options?: any): Promise<string>;
