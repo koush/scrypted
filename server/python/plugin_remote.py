de5d5bf49a97fff779a9b1249ce515e91d37845d
Brett Jia <dev.bjia56@gmail.com>
2024-08-13 22:41:39 -0400

    server: add unzipped path to python module lookup path

--- server/python/plugin_remote.py
+++ server/python/plugin_remote.py
@@ -799,6 +799,8 @@ class PluginRemote:
                 print(str_requirements)
 
             sys.path.insert(0, zipPath)
+            if not debug:
+                sys.path.insert(0, plugin_zip_paths.get("unzipped_path"))
             sys.path.insert(0, pip_target)
 
         self.systemManager = SystemManager(self.api, self.systemState)
