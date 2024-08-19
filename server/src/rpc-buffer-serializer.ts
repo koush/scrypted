731744afbc066969760d0c11f5f6c9ea4d2e6f36
Koushik Dutta <koushd@gmail.com>
2024-08-19 09:47:51 -0700

    server: warn on base64 serialization

--- server/src/rpc-buffer-serializer.ts
+++ server/src/rpc-buffer-serializer.ts
@@ -2,9 +2,13 @@ import { RpcSerializer } from "./rpc";
 
 export class BufferSerializer implements RpcSerializer {
     serialize(value: Buffer) {
+        console.warn('Using slow buffer serialization. Ensure the peer supports SidebandBufferSerializer.');
+        debugger;
         return value.toString('base64');
     }
     deserialize(serialized: any) {
+        console.warn('Using slow buffer deserialization. Ensure the peer supports SidebandBufferSerializer.');
+        debugger;
         return Buffer.from(serialized, 'base64');
     }
 }
