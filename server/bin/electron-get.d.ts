4290eb0abb952302aeccbca8a2a727ef36774aa5
Koushik Dutta <koushd@gmail.com>
2024-08-24 18:11:39 -0700

    server: remove electron

--- server/bin/electron-get.d.ts
+++
@@ -1,3 +0,0 @@
-export declare declare const version: string;
-export declare async function installElectron(): Promise<void>;
-export declare const electronBin: string;
--- server/src/plugin/runtime/runtime-host.ts
+++ server/src/plugin/runtime/runtime-host.ts
@@ -1,6 +1,5 @@
 import type { ScryptedRuntime } from "../../runtime";
 import { CustomRuntimeWorker } from "./custom-worker";
-import { ElectronForkWorker } from "./electron-worker";
 import { NodeForkWorker } from "./node-fork-worker";
 import { PythonRuntimeWorker } from "./python-worker";
 import type { RuntimeWorker, RuntimeWorkerOptions } from "./runtime-worker";
@@ -13,13 +12,6 @@ export function getBuiltinRuntimeHosts() {
     pluginHosts.set('custom', (_, pluginId, options, runtime) => new CustomRuntimeWorker(pluginId, options, runtime));
     pluginHosts.set('python', (_, pluginId, options) => new PythonRuntimeWorker(pluginId, options));
     pluginHosts.set('node', (mainFilename, pluginId, options) => new NodeForkWorker(mainFilename, pluginId, options));
-
-    // safe
-    pluginHosts.set('electron', (mainFilename, pluginId, options, runtime) => new ElectronForkWorker(mainFilename, pluginId, options, runtime, 'default'));
-    // mostly safe
-    pluginHosts.set('electron-webgl', (mainFilename, pluginId, options, runtime) => new ElectronForkWorker(mainFilename, pluginId, options, runtime, 'webgl'));
-    // there be dragons
-    pluginHosts.set('electron-webgpu', (mainFilename, pluginId, options, runtime) => new ElectronForkWorker(mainFilename, pluginId, options, runtime, 'webgpu'));
 
     return pluginHosts;
 }
