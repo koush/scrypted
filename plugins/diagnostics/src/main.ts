cafb6944f5aa74660f6088e1eaf7d6d631066ac6
Koushik Dutta <koushd@gmail.com>
2024-09-10 14:34:03 -0700

    diagnostics: stream validation

--- plugins/diagnostics/src/main.ts
+++ plugins/diagnostics/src/main.ts
@@ -1,24 +1,38 @@
+import sharp from 'sharp';
 import net from 'net';
 import fs from 'fs';
 import os from 'os';
-import sdk, { OnOff, ScryptedDeviceBase, ScryptedDeviceType, ScryptedInterface, Setting, Settings } from '@scrypted/sdk';
+import sdk, { Camera, MediaObject, MotionSensor, OnOff, ScryptedDevice, ScryptedDeviceBase, ScryptedDeviceType, ScryptedInterface, Setting, Settings, VideoCamera } from '@scrypted/sdk';
 import { StorageSettings } from '@scrypted/sdk/storage-settings';
 import { httpFetch, httpFetchParseIncomingMessage } from '../../../server/src/fetch/http-fetch';
 
 class DiagnosticsPlugin extends ScryptedDeviceBase implements Settings {
     storageSettings = new StorageSettings(this, {
+        testDevice: {
+            group: 'Device',
+            title: 'Test Device',
+            type: 'device',
+            deviceFilter: `type === '${ScryptedDeviceType.Camera}' || type === '${ScryptedDeviceType.Doorbell}'`,
+            immediate: true,
+        },
+        validateDevice: {
+            console: true,
+            group: 'Device',
+            title: 'Validate Device',
+            description: 'Validate the device configuration.',
+            type: 'button',
+            onPut: async () => {
+                this.validateDevice();
+            },
+        },
         validateSystem: {
+            console: true,
+            group: 'System',
             title: 'Validate System',
             description: 'Validate the system configuration.',
             type: 'button',
             onPut: () => this.validateSystem(),
         },
-        testDevice: {
-            title: 'Test Device',
-            type: 'device',
-            deviceFilter: `type === '${ScryptedDeviceType.Camera}' || type === '${ScryptedDeviceType.Doorbell}'`,
-            immediate: true,
-        }
     });
 
     loggedMotion = new Map<string, number>();
@@ -58,6 +72,71 @@ class DiagnosticsPlugin extends ScryptedDeviceBase implements Settings {
         }
     }
 
+    async validateDevice() {
+        const device = this.storageSettings.values.testDevice as ScryptedDevice & Camera & VideoCamera & MotionSensor;
+
+        this.console.log(''.padEnd(80, '='));
+        this.console.log(`Device Validation: ${device?.name}`);
+        this.console.log(''.padEnd(80, '='));
+
+        await this.validate('Device Selected', async () => {
+            if (!device)
+                throw new Error('Select a device in the Settings UI.');
+        });
+
+        if (!device)
+            return;
+
+        await this.validate('Device Capabilities', async () => {
+            if (!device.interfaces.includes(ScryptedInterface.MotionSensor))
+                throw new Error('Motion Sensor not found.');
+
+            if (device.type === ScryptedDeviceType.Doorbell && !device.interfaces.includes(ScryptedInterface.BinarySensor))
+                throw new Error('Doorbell button not found.');
+        });
+
+        await this.validate('Recent Motion', async () => {
+            const lastMotion = this.loggedMotion.get(device.id);
+            if (!lastMotion)
+                throw new Error('No recent motion detected. Go wave your hand in front of the camera.');
+            if (Date.now() - lastMotion > 8 * 60 * 60 * 1000)
+                throw new Error('Last motion was over 8 hours ago.');
+        });
+
+        const validateMedia = async (stepName: string, mo: MediaObject) => {
+            await this.validate(stepName, async () => {
+                const jpeg = await sdk.mediaManager.convertMediaObjectToBuffer(mo, 'image/jpeg');
+                const metadata = await sharp(jpeg).metadata();
+                if (!metadata.width || !metadata.height || metadata.width < 100 || metadata.height < 100)
+                    throw new Error('Malformed image.');
+            })
+        };
+
+        await validateMedia('Snapshot', await device.takePicture({
+            reason: 'event',
+        }));
+
+        await validateMedia('Local Stream', await device.getVideoStream({
+            destination: 'local',
+        }));
+
+        await validateMedia('Local Recorder Stream', await device.getVideoStream({
+            destination: 'local-recorder',
+        }));
+
+        await validateMedia('Remote Recorder Stream', await device.getVideoStream({
+            destination: 'remote-recorder',
+        }));
+
+        await validateMedia('Remote Stream', await device.getVideoStream({
+            destination: 'remote',
+        }));
+
+        await validateMedia('Low Resolution Stream', await device.getVideoStream({
+            destination: 'low-resolution',
+        }));
+    }
+
     async validateSystem() {
         this.console.log(''.padEnd(80, '='));
         this.console.log('System Validation');
@@ -92,11 +171,11 @@ class DiagnosticsPlugin extends ScryptedDeviceBase implements Settings {
         });
 
         await this.validate('Memory', async () => {
-            if (os.totalmem() < 8 * 1024 * 1024 * 1024)
-                throw new Error('Memory is too low. 8GB is recommended.');
-
-            if (!nvrPlugin)
+            if (!nvrPlugin) {
+                if (os.totalmem() < 8 * 1024 * 1024 * 1024)
+                    throw new Error('Memory is too low. 8GB is recommended.');
                 return;
+            }
 
             if (os.totalmem() < 14 * 1024 * 1024 * 1024)
                 throw new Error('Memory is too low. 16GB is recommended for NVR.');
