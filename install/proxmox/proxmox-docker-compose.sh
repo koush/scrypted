15ac0613118463745af4eb94efd9bc131f932e6c
Koushik Dutta <koushd@gmail.com>
2024-10-03 13:41:31 -0700

    proxmox: add docker compose script

---
+++ install/proxmox/proxmox-docker-compose.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd /root/.scrypted
+
+# always immediately upgrade everything in case there's a broken update.
+# this will also be preferable for troubleshooting via lxc reboot.
+export DEBIAN_FRONTEND=noninteractive
+(apt -y --fix-broken install && dpkg --configure -a && apt -y update && apt -y dist-upgrade) &
+docker compose pull &
+
+# do not daemonize, when it exits, systemd will restart it.
+docker compose up
