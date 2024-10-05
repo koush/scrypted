09e97a2895e16ccb63a2607bf7ca15624b1c6a27
Koushik Dutta <koushd@gmail.com>
2024-10-05 11:56:07 -0700

    lxc-docker: more system maintenance on startup

--- install/proxmox/docker-compose.sh
+++ install/proxmox/docker-compose.sh
@@ -4,16 +4,16 @@ cd /root/.scrypted
 # always immediately upgrade everything in case there's a broken update.
 # this will also be preferable for troubleshooting via lxc reboot.
 export DEBIAN_FRONTEND=noninteractive
-(apt -y --fix-broken install && dpkg --configure -a && apt -y update && apt -y dist-upgrade) &
+(apt -y --fix-broken install && (yes | dpkg --configure -a) && apt -y update && apt -y dist-upgrade) &
 
 # foreground pull if requested.
 if [ -e "volume/.pull" ]
 then
   rm -rf volume/.pull
-  docker compose pull
+  docker compose pull && docker container prune && docker image prune -a
 else
   # always background pull in case there's a broken image.
-  docker compose pull &
+  (docker compose pull && docker container prune && docker image prune -a) &
 fi
 
 # do not daemonize, when it exits, systemd will restart it.
