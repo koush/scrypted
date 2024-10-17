fe7b4792350afaa6ff4b66e060d4d82a826ec406
Koushik Dutta <koushd@gmail.com>
2024-10-17 11:53:09 -0700

    lxc-docker: use docker compose --pull as appropriate

--- install/proxmox/docker-compose.sh
+++ install/proxmox/docker-compose.sh
@@ -10,14 +10,14 @@ export DEBIAN_FRONTEND=noninteractive
 if [ -e "volume/.pull" ]
 then
   rm -rf volume/.pull
-  docker compose pull && docker container prune -f && docker image prune -a -f
+  PULL="--pull"
 else
   # always background pull in case there's a broken image.
-  (docker compose pull && docker container prune -f && docker image prune -a -f) &
+  (sleep 10 && docker compose pull && docker container prune -f && docker image prune -a -f) &
 fi
 
 # do not daemonize, when it exits, systemd will restart it.
 # force a recreate as .env may have changed.
 # furthermore force recreate gets the container back into a known state
 # which is preferable in case the user has made manual changes and then restarts.
-WATCHTOWER_HTTP_API_TOKEN=$(echo $RANDOM | md5sum | head -c 32) docker compose up --force-recreate --abort-on-container-exit
+WATCHTOWER_HTTP_API_TOKEN=$(echo $RANDOM | md5sum | head -c 32) docker compose up --force-recreate --abort-on-container-exit $PULL
