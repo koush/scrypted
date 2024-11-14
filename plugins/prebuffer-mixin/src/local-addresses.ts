ff903fa8911925b63d3b3ce64d4326bae35f3791
Koushik Dutta <koushd@gmail.com>
2024-11-13 18:55:55 -0800

    rebroadcast: fix external urls with ipv6

--- plugins/prebuffer-mixin/src/local-addresses.ts
+++ plugins/prebuffer-mixin/src/local-addresses.ts
@@ -1,25 +1,20 @@
+import net from 'net';
 import sdk from '@scrypted/sdk';
 
 export async function getUrlLocalAdresses(console: Console, url: string) {
-    let urls: string[];
     try {
         const addresses = await sdk.endpointManager.getLocalAddresses();
-        if (addresses) {
-            const [address] = addresses;
-            if (address) {
-                const u = new URL(url);
-                u.hostname = address;
-                url = u.toString();
-            }
-            urls = addresses.map(address => {
-                const u = new URL(url);
-                u.hostname = address;
-                return u.toString();
-            });
-        }
+        if (!addresses)
+            return;
+        const urls = addresses.map(address => {
+            const u = new URL(url);
+            u.hostname = net.isIPv6(address) ? `[${address}]` : address;
+            return u.toString();
+        });
+        return urls;
     }
     catch (e) {
         console.warn('Error determining external addresses. Is Scrypted Server Address configured?', e);
+        return
     }
-    return urls;
 }
