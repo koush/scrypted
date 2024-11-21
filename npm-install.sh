095f80e1f9a2e91ba225c4944f00b389f80143ab
Koushik Dutta <koushd@gmail.com>
2024-11-21 10:04:03 -0800

    dev: update npm-install.sh

--- npm-install.sh
+++ npm-install.sh
@@ -27,7 +27,7 @@ echo "external/werift > npm install"
 npm install
 popd
 
-for directory in ffmpeg-camera rtsp amcrest onvif hikvision unifi-protect webrtc homekit
+for directory in rtsp amcrest onvif hikvision reolink unifi-protect webrtc homekit
 do
     echo "$directory > npm install"
     pushd plugins/$directory
