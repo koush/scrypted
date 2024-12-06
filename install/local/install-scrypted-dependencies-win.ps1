08dac20d7d0ede9c5988be0d3c6b315f6c358500
Long Zheng <long.zheng@gmail.com>
2024-12-06 23:28:32 +1100

    Remove node version debug

--- install/local/install-scrypted-dependencies-win.ps1
+++ install/local/install-scrypted-dependencies-win.ps1
@@ -24,8 +24,6 @@ $SCRYPTED_WINDOWS_PYTHON_VERSION="-3.9"
 # Refresh environment variables for py and npx to work
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
 
-node -v
-
 # Workaround Windows Node no longer creating %APPDATA%\npm which causes npx to fail
 # Fixed in newer versions of NPM but not the one bundled with 20.11.1
 # https://github.com/nodejs/node/issues/53538
