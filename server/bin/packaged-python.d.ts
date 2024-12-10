6299caac208230546a013fd285b92b36a25febe1
Koushik Dutta <koushd@gmail.com>
2024-12-10 09:30:29 -0800

    server: fix typings

--- server/bin/packaged-python.d.ts
+++ server/bin/packaged-python.d.ts
@@ -1,2 +1,2 @@
-export declare declare const version: string;
-export declare async function installScryptedServerRequirements(version?: string, dest?: string, options?: any): Promise<string>;
+export declare const version: string;
+export declare function installScryptedServerRequirements(version?: string, dest?: string, options?: any): Promise<string>;
