8ec6a2583395d6054d3a1197ac9cf45e7b2019f1
Koushik Dutta <koushd@gmail.com>
2024-12-23 11:16:36 -0800

    rebroadcast: add support for rtsp url using cluster address in case scrypted server address is not set

--- plugins/prebuffer-mixin/src/local-addresses.ts
+++ plugins/prebuffer-mixin/src/local-addresses.ts
@@ -2,19 +2,33 @@ import net from 'net';
 import sdk from '@scrypted/sdk';
 
 export async function getUrlLocalAdresses(console: Console, url: string) {
+    let urls: string[];
     try {
         const addresses = await sdk.endpointManager.getLocalAddresses();
         if (!addresses)
             return;
-        const urls = addresses.map(address => {
+        urls = addresses.map(address => {
             const u = new URL(url);
             u.hostname = net.isIPv6(address) ? `[${address}]` : address;
             return u.toString();
         });
-        return urls;
     }
     catch (e) {
         console.warn('Error determining external addresses. Is Scrypted Server Address configured?', e);
-        return
     }
+
+    if (process.env.SCRYPTED_CLUSTER_ADDRESS) {
+        try {
+            const clusterUrl = new URL(url);
+            clusterUrl.hostname = process.env.SCRYPTED_CLUSTER_ADDRESS;
+            const str = clusterUrl.toString();
+            if (!urls.includes(str))
+                urls.push(str);
+        }
+        catch (e) {
+            console.warn('Error determining external addresses. Is Scrypted Cluster Address configured?', e);
+        }
+    }
+
+    return urls;
 }
