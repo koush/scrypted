8e1dfa8174c1c3f275f7f2117918cc842011f2c5
Koushik Dutta <koushd@gmail.com>
2024-12-28 20:35:44 -0800

    core: ensure lxc-docker is updated every boot

--- install/proxmox/docker-compose.sh
+++ install/proxmox/docker-compose.sh
@@ -4,21 +4,15 @@ cd /root/.scrypted
 # always immediately upgrade everything in case there's a broken update.
 # this will also be preferable for troubleshooting via lxc reboot.
 export DEBIAN_FRONTEND=noninteractive
-((yes | dpkg --configure -a) ; apt -y --fix-broken install && apt -y update && apt -y dist-upgrade) &
+yes | dpkg --configure -a
+apt -y --fix-broken install && apt -y update && apt -y dist-upgrade
 
-# foreground pull if requested.
-if [ -e "volume/.pull" ]
-then
-  rm -rf volume/.pull
-  PULL="--pull"
-  (sleep 300 && docker container prune -f && docker image prune -a -f) &
-else
-  # always background pull in case there's a broken image.
-  (sleep 300 && docker compose pull && docker container prune -f && docker image prune -a -f) &
-fi
+# force a pull to ensure we have the latest images.
+# not using --pull always cause that fails everything on network down
+docker compose pull
 
 # do not daemonize, when it exits, systemd will restart it.
 # force a recreate as .env may have changed.
 # furthermore force recreate gets the container back into a known state
 # which is preferable in case the user has made manual changes and then restarts.
-WATCHTOWER_HTTP_API_TOKEN=$(echo $RANDOM | md5sum | head -c 32) docker compose up --force-recreate --abort-on-container-exit $PULL
+WATCHTOWER_HTTP_API_TOKEN=$(echo $RANDOM | md5sum | head -c 32) docker compose up --force-recreate --abort-on-container-exit
