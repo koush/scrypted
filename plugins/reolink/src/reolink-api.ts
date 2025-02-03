cb45a00c25ee84169aa506a8a1803488815748db
apocaliss92 <giangy.ruocco.92@gmail.com>
2025-02-03 17:51:45 +0100

    reolink: Battery cams api fixes (#1719)
    * Battery cams api fixes
    
    * Update with new Sleep class
    
    ---------
    
    Co-authored-by: Gianluca Ruocco <gianluca.ruocco@xarvio.com>

--- plugins/reolink/src/reolink-api.ts
+++ plugins/reolink/src/reolink-api.ts
@@ -492,7 +492,35 @@ export class ReolinkCameraClient {
 
         return {
             batteryPercent: batteryInfoEntry?.batteryPercent,
-            sleep: channelStatusEntry?.sleep === 1,
+            sleeping: channelStatusEntry?.sleep === 1,
         }
+    }
+
+    async getPidActive() {
+        const url = new URL(`http://${this.host}/api.cgi`);
+
+        const body = [
+            {
+                cmd: "GetEvents",
+                action: 0,
+                param: { channel: this.channelId }
+            },
+        ];
+
+        const response = await this.requestWithLogin({
+            url,
+            responseType: 'json',
+            method: 'POST',
+        }, this.createReadable(body));
+
+        const error = response.body?.find(elem => elem.error)?.error;
+        if (error) {
+            this.console.error('error during call to getEvents', error);
+        }
+
+        return {
+            value: !!response.body?.[0]?.value?.ai?.other?.alarm_state,
+            data: response.body,
+        };
     }
 }
