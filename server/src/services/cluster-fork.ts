adcdd18497dce176cadcd25a03c053dd1924df3b
Koushik Dutta <koushd@gmail.com>
2025-02-05 11:50:51 -0800

    server: add cluster worker address

--- server/src/services/cluster-fork.ts
+++ server/src/services/cluster-fork.ts
@@ -109,6 +109,7 @@ export class ClusterForkService {
                 labels: worker.labels,
                 forks: [...worker.forks] as ClusterFork[],
                 mode: worker.mode,
+                address: worker.address,
             };
         }
         return ret;
