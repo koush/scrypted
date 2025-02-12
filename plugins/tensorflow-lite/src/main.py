833ecb721fa8d4a2a835921be3f22167c2598e73
Koushik Dutta <koushd@gmail.com>
2025-02-12 13:39:55 -0800

    tensorflow-lite: pass through forked flag

--- plugins/tensorflow-lite/src/main.py
+++ plugins/tensorflow-lite/src/main.py
@@ -1,4 +1,8 @@
 from tflite import TensorFlowLitePlugin
+import predict
 
 def create_scrypted_plugin():
     return TensorFlowLitePlugin()
+
+async def fork():
+    return predict.Fork(TensorFlowLitePlugin)
