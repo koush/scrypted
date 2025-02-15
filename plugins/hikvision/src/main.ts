98e2d458e79f6e5ffb2acf3127e7d5a09fac981a
Mehmet Bayram <akifbayram@gmail.com>
2025-02-15 18:08:06 -0500

    fix: push DeviceProvider interface

--- plugins/hikvision/src/main.ts
+++ plugins/hikvision/src/main.ts
@@ -472,6 +472,9 @@ export class HikvisionCamera extends RtspSmartCamera implements Camera, Intercom
         if (this.hasSmartDetection)
             interfaces.push(ScryptedInterface.ObjectDetector);
 
+        if (this.hasAlarm || this.hasSupplementLight)
+            interfaces.push(ScryptedInterface.DeviceProvider);
+
         this.provider.updateDevice(this.nativeId, this.name, interfaces, type);
     }
 
