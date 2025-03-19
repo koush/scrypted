21c771d50f553442b112f2f544ec582912914aee
Koushik Dutta <koushd@gmail.com>
2025-03-18 19:08:34 -0700

    common: deferred dispose

--- server/src/deferred.ts
+++ server/src/deferred.ts
@@ -2,6 +2,12 @@ export class Deferred<T> {
     finished = false;
     resolve!: (value: T) => this;
     reject!: (error: Error) => this;
+
+    [Symbol.dispose]() {
+        if (!this.finished)
+            this.reject(new Error('deferred disposed without being resolved'));
+    }
+
     async resolvePromise(p: Promise<T>) {
         try {
             this.resolve(await p);
