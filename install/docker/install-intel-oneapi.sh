247f860a23d87ddceb8fd60fd1a61564a300a13d
Koushik Dutta <koushd@gmail.com>
2025-06-12 21:21:57 -0700

    intel: fix curl/gpg interaction maybe

--- install/docker/install-intel-oneapi.sh
+++ install/docker/install-intel-oneapi.sh
@@ -1,8 +1,15 @@
-# download the key to system keyring
-curl -1sLf https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB | gpg --dearmor --yes --output /usr/share/keyrings/oneapi-archive-keyring.gpg
+if [ "$(uname -m)" = "x86_64" ]
+then
+    # download the key to system keyring
+    curl -1sLf https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB | gpg --dearmor --yes --output /usr/share/keyrings/oneapi-archive-keyring.gpg
 
-# add signed entry to apt sources and configure the APT client to use Intel repository:
-echo "deb [signed-by=/usr/share/keyrings/oneapi-archive-keyring.gpg] https://apt.repos.intel.com/oneapi all main" | tee /etc/apt/sources.list.d/oneAPI.list
+    # add signed entry to apt sources and configure the APT client to use Intel repository:
+    echo "deb [signed-by=/usr/share/keyrings/oneapi-archive-keyring.gpg] https://apt.repos.intel.com/oneapi all main" | tee /etc/apt/sources.list.d/oneAPI.list
 
-apt -y update
-apt -y install intel-oneapi-mkl-sycl-blas intel-oneapi-runtime-dnnl intel-oneapi-runtime-compilers
+    apt -y update
+    apt -y install intel-oneapi-mkl-sycl-blas intel-oneapi-runtime-dnnl intel-oneapi-runtime-compilers
+else
+    echo "NVIDIA graphics will not be installed on this architecture."
+fi
+
+exit 0
