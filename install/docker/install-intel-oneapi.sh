17f42762e72b07cb7a17f4638bad996c6bdd34a2
Koushik Dutta <koushd@gmail.com>
2025-06-12 20:08:08 -0700

    install: prep intel llm deps

---
+++ install/docker/install-intel-oneapi.sh
@@ -0,0 +1,9 @@
+# download the key to system keyring
+curl -L https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB \
+| gpg --dearmor | tee /usr/share/keyrings/oneapi-archive-keyring.gpg > /dev/null
+
+# add signed entry to apt sources and configure the APT client to use Intel repository:
+echo "deb [signed-by=/usr/share/keyrings/oneapi-archive-keyring.gpg] https://apt.repos.intel.com/oneapi all main" | tee /etc/apt/sources.list.d/oneAPI.list
+
+apt -y update
+apt -y install intel-oneapi-mkl-sycl-blas intel-oneapi-runtime-dnnl intel-oneapi-runtime-compilers
