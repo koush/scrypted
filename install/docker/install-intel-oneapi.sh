67448512561ab97dbaa96d2bb31b87a6b58a8078
Koushik Dutta <koushd@gmail.com>
2025-06-12 21:13:13 -0700

    intel: add logging

--- install/docker/install-intel-oneapi.sh
+++ install/docker/install-intel-oneapi.sh
@@ -1,6 +1,6 @@
 # download the key to system keyring
 curl -L https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB \
-| gpg --dearmor | tee /usr/share/keyrings/oneapi-archive-keyring.gpg > /dev/null
+| gpg --dearmor | tee /usr/share/keyrings/oneapi-archive-keyring.gpg
 
 # add signed entry to apt sources and configure the APT client to use Intel repository:
 echo "deb [signed-by=/usr/share/keyrings/oneapi-archive-keyring.gpg] https://apt.repos.intel.com/oneapi all main" | tee /etc/apt/sources.list.d/oneAPI.list
