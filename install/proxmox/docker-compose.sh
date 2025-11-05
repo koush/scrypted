d515cc47d055cc7c4132170bd5ef16286186412e
Koushik Dutta <koushd@gmail.com>
2025-11-05 08:52:26 -0800

    core: temporarily disable lxc-docker abort-on-container-exit

--- install/proxmox/docker-compose.sh
+++ install/proxmox/docker-compose.sh
@@ -4,6 +4,9 @@ cd /root/.scrypted
 # always immediately upgrade everything in case there's a broken update.
 # this will also be preferable for troubleshooting via lxc reboot.
 export DEBIAN_FRONTEND=noninteractive
+
+# auto updates may break the system?
+# watchtower stopped working after a docker update, so disabling for now.
 # yes | dpkg --configure -a
 # apt -y --fix-broken install && apt -y update && apt -y dist-upgrade
 
@@ -30,4 +33,7 @@ docker compose pull
 # force a recreate as .env may have changed.
 # furthermore force recreate gets the container back into a known state
 # which is preferable in case the user has made manual changes and then restarts.
-WATCHTOWER_HTTP_API_TOKEN=$(echo $RANDOM | md5sum | head -c 32) docker compose up --force-recreate --abort-on-container-exit
+WATCHTOWER_HTTP_API_TOKEN=$(echo $RANDOM | md5sum | head -c 32) docker compose up --force-recreate
+
+# abort on container exit is problematic if watchtower is the one that aborts.
+# WATCHTOWER_HTTP_API_TOKEN=$(echo $RANDOM | md5sum | head -c 32) docker compose up --force-recreate --abort-on-container-exit
