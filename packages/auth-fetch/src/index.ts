181d0fdc9268459d778310b5c0729620929c71ce
Koushik Dutta <koushd@gmail.com>
2026-03-31 08:38:02 -0700

    packages: expose http fetch

--- packages/auth-fetch/src/index.ts
+++ packages/auth-fetch/src/index.ts
@@ -1,8 +1,9 @@
-import { createAuthFetch } from "./auth-fetch";
-import { httpFetch, httpFetchParseIncomingMessage } from '../../../server/src/fetch/http-fetch';
-import type { Readable } from "stream";
 import type { IncomingMessage } from "http";
+import type { Readable } from "stream";
 import { domFetch, domFetchParseIncomingMessage } from "../../../server/src/fetch";
+import { httpFetch, httpFetchParseIncomingMessage } from '../../../server/src/fetch/http-fetch';
+import { createAuthFetch } from "./auth-fetch";
+export { httpFetch } from '../../../server/src/fetch/http-fetch';
 
 function init() {
     try {
